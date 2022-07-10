4b92c3dbcd6d605a15f6652b186a8bfca8933b4d
mrcreepysos <59087849+mrcreepysos@users.noreply.github.com>
2022-07-10 18:21:28 +0300

    1.32
    simplify bleedout timer decrease code (thanks hop)

--- lua/playerdamage.lua
+++ lua/playerdamage.lua
@@ -46,64 +46,8 @@ function PlayerDamage:restore_health(health_restored, is_static, chk_health_rati
 end
 
 -- bring back decreasing bleedout timer based on the amount of downs
--- apprently using a posthook breaks the revive health steps for some reason so gotta replace the whole shit
-function PlayerDamage:revive(silent)
-	if Application:digest_value(self._revives, false) == 0 then
-		self._revive_health_multiplier = nil
-
-		return
+Hooks:PreHook(PlayerDamage, "revive", "eclipse_revive", function(self)
+	if not self:arrested() then
+	  self._down_time = math.max(tweak_data.player.damage.DOWNED_TIME_MIN, self._down_time - tweak_data.player.damage.DOWNED_TIME_DEC)
 	end
-
-	local arrested = self:arrested()
-
-	managers.player:set_player_state("standard")
-	managers.player:remove_copr_risen_cooldown()
-
-	if not silent then
-		PlayerStandard.say_line(self, "s05x_sin")
-	end
-
-	self._bleed_out = false
-	self._incapacitated = nil
-	self._downed_timer = nil
-	self._downed_start_time = nil
-
-	if not arrested then
-		self:set_health(self:_max_health() * tweak_data.player.damage.REVIVE_HEALTH_STEPS[self._revive_health_i] * (self._revive_health_multiplier or 1) * managers.player:upgrade_value("player", "revived_health_regain", 1))
-		self:set_armor(self:_max_armor())
-
-		self._revive_health_i = math.min(#tweak_data.player.damage.REVIVE_HEALTH_STEPS, self._revive_health_i + 1)
-		self._down_time = math.max(tweak_data.player.damage.DOWNED_TIME_MIN, self._down_time - tweak_data.player.damage.DOWNED_TIME_DEC) --This is a real PD:TH moment
-		self._revive_miss = 2
-	end
-
-	self:_regenerate_armor()
-	managers.hud:set_player_health({
-		current = self:get_real_health(),
-		total = self:_max_health(),
-		revives = Application:digest_value(self._revives, false)
-	})
-	self:_send_set_health()
-	self:_set_health_effect()
-	managers.hud:pd_stop_progress()
-
-	self._revive_health_multiplier = nil
-
-	self._listener_holder:call("on_revive")
-
-	if managers.player:has_inactivate_temporary_upgrade("temporary", "revived_damage_resist") then
-		managers.player:activate_temporary_upgrade("temporary", "revived_damage_resist")
-	end
-
-	if managers.player:has_inactivate_temporary_upgrade("temporary", "increased_movement_speed") then
-		managers.player:activate_temporary_upgrade("temporary", "increased_movement_speed")
-	end
-
-	if managers.player:has_inactivate_temporary_upgrade("temporary", "swap_weapon_faster") then
-		managers.player:activate_temporary_upgrade("temporary", "swap_weapon_faster")
-	end
-
-	if managers.player:has_inactivate_temporary_upgrade("temporary", "reload_weapon_faster") then
-		managers.player:activate_temporary_upgrade("temporary", "reload_weapon_faster")
-	end
-end
+  end)
