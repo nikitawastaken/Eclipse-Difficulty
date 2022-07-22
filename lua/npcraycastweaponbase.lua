6fffa8afc91c361b0175fa1d1e00f59bf964df99
mrcreepysos <59087849+mrcreepysos@users.noreply.github.com>
2022-07-22 16:13:10 +0300

    2.42

--- lua/npcraycastweaponbase.lua
+++ lua/npcraycastweaponbase.lua
@@ -3,7 +3,7 @@ Hooks:PreHook(NPCRaycastWeaponBase, "_fire_raycast", "_eclipse_fire_raycast", fu
     local __enemies = managers.enemy:all_enemies()
     if shoot_player then
         for k,v_key in ipairs(__hostages) do
-            table.insert(self._setup.ignore_units, __enemies[v_key].unit)
+            if __enemies[v_key] then table.insert(self._setup.ignore_units, __enemies[v_key].unit) end
         end
     end
 end)
