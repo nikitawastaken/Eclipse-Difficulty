1afb1f9cd288f23ec29ed5021feff4d9aff46c47
Rockymoto517 <Rockymoto517@gmail.com>
2022-08-19 20:45:54 -0500

    Code formatting and documentation

--- lua/sentrygunbase.lua
+++ lua/sentrygunbase.lua
@@ -2,21 +2,25 @@
 -- Enemies will still shoot sentries but they won't actively path towards them
 Hooks:PostHook(SentryGunBase, "setup", "sh_setup", SentryGunBase.unregister)
 
+-- Create table for sixth sense timing data
 Hooks:PostHook(SentryGunBase, "init", "eclipse_init",
 function(self, unit)
     self._state_data = self._state_data or {}
 end)
 
+-- Check for sixth sense every tick
 Hooks:PostHook(SentryGunBase, "update", "eclipse_update",
 function(self, unit, t, dt)
     self:_update_omniscience(t, dt)
 end)
 
+-- Create new sixth sense function
 function SentryGunBase:_update_omniscience(t, dt)
 	if not tweak_data.player.omniscience then
 		if self._state_data.omniscience_t then
 			self._state_data.omniscience_t = nil
 		end
+
 		return
 	end
 
@@ -28,14 +32,16 @@ function SentryGunBase:_update_omniscience(t, dt)
 		for _, unit in ipairs(sensed_targets) do
 			if alive(unit) and not unit:base():char_tweak().is_escort then
 			    self._state_data.omniscience_units_detected = self._state_data.omniscience_units_detected or {}
+
 			    if not self._state_data.omniscience_units_detected[unit:key()] or self._state_data.omniscience_units_detected[unit:key()] <= t then
 			        self._state_data.omniscience_units_detected[unit:key()] = t + tweak_data.player.omniscience.target_resense_t
-				managers.game_play_central:auto_highlight_enemy(unit, true)
+
+				    managers.game_play_central:auto_highlight_enemy(unit, true)
+
 				    break
                 end
 			end
 		end
-
 		self._state_data.omniscience_t = t + tweak_data.player.omniscience.interval_t
 	end
 end
