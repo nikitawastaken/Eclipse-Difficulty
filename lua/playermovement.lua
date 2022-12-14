57dbec8772152fa3991effbc3dfc027e9b481004
mrcreepysos <mr.creepy@yandex.ru>
2022-12-14 15:53:38 +0300

    fix a crash

--- lua/playermovement.lua
+++ lua/playermovement.lua
@@ -1,3 +1,5 @@
 Hooks:PostHook(PlayerMovement, "init", "eclipse_init", function(self)
-	self._rally_skill_data.range_sq = 490000
+	if managers.player:has_category_upgrade("player", "morale_boost") or managers.player:has_category_upgrade("cooldown", "long_dis_revive") then
+		self._rally_skill_data.range_sq = 490000
+	end
 end)
