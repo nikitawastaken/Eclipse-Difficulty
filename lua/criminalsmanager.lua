5cb6aa544b2302e7f6cc3981fee86b4a0f9e618a
mrcreepysos <mr.creepy@yandex.ru>
2023-03-26 18:10:31 +0300

    fix loud tutorial softlock

--- lua/criminalsmanager.lua
+++ lua/criminalsmanager.lua
@@ -1,1 +1,5 @@
-CriminalsManager.MAX_NR_TEAM_AI = 1
+if Global.level_data and (Global.level_data.level_id == "short2_stage1" or Global.level_data.level_id == "short2_stage2b") then
+    CriminalsManager.MAX_NR_TEAM_AI = 2
+else
+    CriminalsManager.MAX_NR_TEAM_AI = 1
+end
