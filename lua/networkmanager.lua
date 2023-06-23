8ff51f58d6a836ef78188ddeb053e4ff23f881be
mrcreepysos <mr.creepy@yandex.ru>
2023-06-23 16:45:45 +0300

    update for epic mm

---
+++ lua/networkmanager.lua
@@ -0,0 +1,2 @@
+NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY .. "_eclipse_dev"
+NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY .. "_eclipse_dev"
