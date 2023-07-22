168fdbaec281f7026f592d79298b8b66d8cfde95
mrcreepysos <mrcreepysos@users.noreply.github.com>
2023-07-22 17:11:15 +0000

    Code formatting

--- lua/playercamera.lua
+++ lua/playercamera.lua
@@ -1,5 +1,13 @@
 -- aimpunch
 local play_shaker_original = PlayerCamera.play_shaker
 function PlayerCamera:play_shaker(effect, amplitude, ...)
-	return play_shaker_original(self, effect, effect == "player_bullet_damage" and self._damage_bullet_shake_multiplier or effect == "player_bullet_damage_strong" and self._damage_bullet_shake_multiplier or effect == "player_bullet_damage_knock_out" and self._damage_bullet_shake_multiplier or amplitude, ...)
+	return play_shaker_original(
+		self,
+		effect,
+		effect == "player_bullet_damage" and self._damage_bullet_shake_multiplier
+			or effect == "player_bullet_damage_strong" and self._damage_bullet_shake_multiplier
+			or effect == "player_bullet_damage_knock_out" and self._damage_bullet_shake_multiplier
+			or amplitude,
+		...
+	)
 end
--- lua/playerdamage.lua
+++ lua/playerdamage.lua
@@ -24,11 +24,11 @@ function PlayerDamage:damage_bullet(attack_data)
 	local damage_info = {
 		result = {
 			variant = "bullet",
-			type = "hurt"
+			type = "hurt",
 		},
 		attacker_unit = attack_data.attacker_unit,
 		attack_dir = attack_data.attacker_unit and attack_data.attacker_unit:movement():m_pos() - self._unit:movement():m_pos() or Vector3(1, 0, 0),
-		pos = mvector3.copy(self._unit:movement():m_head_pos())
+		pos = mvector3.copy(self._unit:movement():m_head_pos()),
 	}
 	local pm = managers.player
 	local dmg_mul = pm:damage_reduction_skill_multiplier("bullet")
