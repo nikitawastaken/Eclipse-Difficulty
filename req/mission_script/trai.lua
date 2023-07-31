517ee7df2dbfa7f258aa3c3576edf001732f3e33
mrcreepysos <mr.creepy@yandex.ru>
2023-07-31 21:48:24 +0300

    nuke ponrs from lost transit (heist still ass tho)

---
+++ req/mission_script/trai.lua
@@ -0,0 +1,12 @@
+return {
+    [103501] = {
+        values = {
+            enabled = false
+        }
+    },
+    [103051] = {
+        values = {
+            enabled = false
+        }
+    }
+}
