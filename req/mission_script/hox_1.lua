9c6390e9892e953b816350dce6f781d708349e2f
mrcreepysos <mr.creepy@yandex.ru>
2023-07-31 05:27:06 +0300

    unfuck hoxd1 garage difficulty

--- req/mission_script/hox_1.lua
+++ req/mission_script/hox_1.lua
@@ -3,5 +3,10 @@ return {
     [100580] = {
         ponr = 600,
         ponr_player_mul = {1.8, 1.5, 1.3, 1.2}
+    },
+    [100124] = {
+        values = {
+            difficulty = 0.75
+        }
     }
 }
