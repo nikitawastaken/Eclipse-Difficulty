b32d62ab63a2d9d95012098c0bf1592eb5a478ff
nikita <59087849+mrcreepysos@users.noreply.github.com>
2023-08-18 07:14:55 +0300

    5.2
    drastically increase ponr length on white house

--- req/mission_script/vit.lua
+++ req/mission_script/vit.lua
@@ -11,8 +11,8 @@ return {
 		}
 	},
 	[100022] = {
-		ponr = 1200,
-        ponr_player_mul = {2, 1.5, 1.25, 1}
+		ponr = 1500,
+        ponr_player_mul = {3, 2, 1.25, 1}
 	},
 	[103360] = {
 		values = {
