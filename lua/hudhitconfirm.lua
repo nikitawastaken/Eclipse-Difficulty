d331deb2fcd24279f7908da44bee773b45848edc
rockymoto517 <rockymoto517@users.noreply.github.com>
2023-08-27 15:20:33 +0000

    Code formatting

--- lua/hudhitconfirm.lua
+++ lua/hudhitconfirm.lua
@@ -1,5 +1,5 @@
 Hooks:PostHook(HUDHitConfirm, "init", "eclipse_hudhitconfirm_init", function(self)
-    self._ff_confirm = self._hud_panel:bitmap({
+	self._ff_confirm = self._hud_panel:bitmap({
 		texture = "guis/textures/pd2/hitconfirm",
 		name = "hit_confirm",
 		halign = "center",
@@ -7,13 +7,13 @@ Hooks:PostHook(HUDHitConfirm, "init", "eclipse_hudhitconfirm_init", function(sel
 		layer = 0,
 		blend_mode = "add",
 		valign = "center",
-		color = Color.red
+		color = Color.red,
 	})
 
-    self._ff_confirm:set_center(self._hud_panel:w() / 2, self._hud_panel:h() / 2)
+	self._ff_confirm:set_center(self._hud_panel:w() / 2, self._hud_panel:h() / 2)
 end)
 
 function HUDHitConfirm:on_ff_confirmed()
-    self._ff_confirm:stop()
-    self._ff_confirm:animate(callback(self, self, "_animate_show"), callback(self, self, "show_done"), 0.25, 1)
+	self._ff_confirm:stop()
+	self._ff_confirm:animate(callback(self, self, "_animate_show"), callback(self, self, "show_done"), 0.25, 1)
 end
--- lua/teamaidamage.lua
+++ lua/teamaidamage.lua
@@ -2,19 +2,19 @@
 TeamAIDamage.is_friendly_fire = PlayerDamage.is_friendly_fire
 
 Hooks:PostHook(TeamAIDamage, "damage_bullet", "eclipse_teamai_damage_bullet", function(self, attack_data)
-    if attack_data.attacker_unit == managers.player:player_unit() then
+	if attack_data.attacker_unit == managers.player:player_unit() then
 		managers.hud:on_ff_confirmed()
 	end
 end)
 
 Hooks:PostHook(TeamAIDamage, "damage_melee", "eclipse_teamai_damage_melee", function(self, attack_data)
-    if attack_data.attacker_unit == managers.player:player_unit() then
+	if attack_data.attacker_unit == managers.player:player_unit() then
 		managers.hud:on_ff_confirmed()
 	end
 end)
 
 Hooks:PostHook(TeamAIDamage, "damage_fire", "eclipse_teamai_damage_fire", function(self, attack_data)
-    if attack_data.attacker_unit == managers.player:player_unit() then
+	if attack_data.attacker_unit == managers.player:player_unit() then
 		managers.hud:on_ff_confirmed()
 	end
 end)
