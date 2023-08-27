eed2d0906107df7adbcdf1719d05b73273ca1114
Rockymoto517 <Rockymoto517@gmail.com>
2023-08-27 09:30:43 -0500

    teamai friendly fire fixes

--- lua/teamaidamage.lua
+++ lua/teamaidamage.lua
@@ -1,2 +1,8 @@
 -- Add missing friendly fire check
 TeamAIDamage.is_friendly_fire = PlayerDamage.is_friendly_fire
+
+Hooks:PostHook(TeamAIDamage, "damage_bullet", "eclipse_teamai_damage_bullet", function(self, attack_data)
+    if attack_data.attacker_unit == managers.player:player_unit() then
+		managers.hud:on_hit_confirmed()
+	end
+end)
