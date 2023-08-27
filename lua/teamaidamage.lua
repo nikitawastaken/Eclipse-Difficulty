d81666d6a90e14435b31f989b93dd8009ffd805c
Rockymoto517 <Rockymoto517@gmail.com>
2023-08-27 10:17:05 -0500

    More teamai ff bugfixes + unique ff hitmarkers

--- lua/teamaidamage.lua
+++ lua/teamaidamage.lua
@@ -2,7 +2,24 @@
 TeamAIDamage.is_friendly_fire = PlayerDamage.is_friendly_fire
 
 Hooks:PostHook(TeamAIDamage, "damage_bullet", "eclipse_teamai_damage_bullet", function(self, attack_data)
+<<<<<<< HEAD
+    if attack_data.attacker_unit == managers.player:player_unit() then
+		managers.hud:on_ff_confirmed()
+	end
+end)
+
+Hooks:PostHook(TeamAIDamage, "damage_melee", "eclipse_teamai_damage_melee", function(self, attack_data)
+    if attack_data.attacker_unit == managers.player:player_unit() then
+		managers.hud:on_ff_confirmed()
+	end
+end)
+
+Hooks:PostHook(TeamAIDamage, "damage_fire", "eclipse_teamai_damage_fire", function(self, attack_data)
+    if attack_data.attacker_unit == managers.player:player_unit() then
+		managers.hud:on_ff_confirmed()
+=======
 	if attack_data.attacker_unit == managers.player:player_unit() then
 		managers.hud:on_hit_confirmed()
+>>>>>>> 0edead7dbd671992e76f66bd2ee828de7cccf2ce
 	end
 end)
