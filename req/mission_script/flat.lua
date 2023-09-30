c95ced73805da8fd61d32f3424bc76615e44ec2c
nikita <59087849+mrcreepysos@users.noreply.github.com>
2023-10-01 01:47:42 +0300

    panic room ASS changes
    Co-Authored-By: moon <108492539+erushinj@users.noreply.github.com>

--- req/mission_script/flat.lua
+++ req/mission_script/flat.lua
@@ -1,4 +1,24 @@
+	-- more ASS edits
+
 return {
+	-- more oppressive open door amounts
+	[103455] = {
+		values = {
+			amount = 0,
+			amount_random = 2,
+		},
+	},
+	[103490] = {
+		values = {
+			amount_random = 0,
+		},
+	},
+	[103618] = {
+		values = {
+			amount = 0,
+			amount_random = 3,
+		},
+	},
 	-- Disable roof/stairs reinforcement
 	[102501] = {
 		values = {
@@ -10,6 +30,12 @@ return {
 			enabled = false
 		}
 	},
+	-- re-allow sniper respawns
+	[104556] = {
+		values = {
+			enabled = false,
+		},
+	},
 	-- reenable far sniper
     [101521] = {
 		values = {
@@ -18,8 +44,128 @@ return {
 	},
     [101599] = {
 		values = {
+			trigger_times = 0,
 			enabled = true
 		}
+	},
+	[103143] = {
+		values = {
+			trigger_times = 0,
+		},
+	},
+	[103134] = {
+		values = {
+			trigger_times = 0,
+		},
+	},
+	[103137] = {
+		values = {
+			trigger_times = 0,
+		},
+	},
+	[103130] = {
+		values = {
+			trigger_times = 0,
+		},
+	},
+	[103126] = {
+		values = {
+			trigger_times = 0,
+		},
+	},
+	[102833] = {
+		values = {
+			trigger_times = 0,
+		},
+	},
+	[101801] = {
+		values = {
+			trigger_times = 0,
+		},
+	},
+	[102614] = {
+		values = {
+			trigger_times = 0,
+		},
+	},
+	[102612] = {
+		values = {
+			trigger_times = 0,
+		},
+	},
+	[103148] = {
+		values = {
+			trigger_times = 0,
+		},
+	},
+	[103168] = {
+		values = {
+			trigger_times = 0,
+		},
+	},
+	[100793] = {
+		values = {
+			trigger_times = 0,
+		},
+	},
+	[100645] = {
+		values = {
+			trigger_times = 0,
+		},
+	},
+	[103111] = {
+		values = {
+			trigger_times = 0,
+		},
+	},
+	[100693] = {
+		values = {
+			trigger_times = 0,
+		},
+	},
+	-- slow down roof spawns, these are really fuckng annoying
+	[104650] = {
+		values = {
+			interval = 40,
+		},
+	},
+	[100504] = {
+		values = {
+			interval = 40,
+		},
+	},
+	[100505] = {
+		values = {
+			interval = 40,
+		},
+	},
+	[100509] = {
+		values = {
+			interval = 40,
+		},
+	},
+	[100396] = {
+		values = {
+			interval = 40,
+		},
+	},
+	-- disable panic room reenforce (sh disables the other two points, and this heist doesnt really need it)
+	[103348] = {
+		values = {
+			enabled = false,
+		},
+	},
+	-- ambush line fix ?  hasnt been working for me since forever
+	[100501] = {
+		values = {
+			use_play_func = true,
+		},
+	},
+	-- reduce delay on mask up when ambushed (this triggers loud)
+	[102329] = {
+		on_executed = {
+			{ id = 102332, delay = 1.5, },
+		},
 	},
 	-- reenable alleyway drop
     [102261] = {
