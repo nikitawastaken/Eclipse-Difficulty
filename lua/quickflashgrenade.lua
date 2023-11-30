3a85bfd853fb5a324a3d7fcfda05cdbc5b62480e
mrcreepysos <mrcreepysos@users.noreply.github.com>
2023-11-30 21:38:35 +0000

    Code formatting

--- lua/quickflashgrenade.lua
+++ lua/quickflashgrenade.lua
@@ -1,5 +1,5 @@
 -- Fix flash grenade timer
-Hooks:PreHook(QuickFlashGrenade, "init", "sh_init", function ()
+Hooks:PreHook(QuickFlashGrenade, "init", "sh_init", function()
 	QuickFlashGrenade.States[3][2] = QuickFlashGrenade.States[3][2] or tweak_data.group_ai.flash_grenade.timer
 end)
 
