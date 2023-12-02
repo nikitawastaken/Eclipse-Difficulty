4399f3b6311655949f7a48fa3794dd20fd8ddf1e
nikita <59087849+mrcreepysos@users.noreply.github.com>
2023-12-03 01:45:49 +0300

    5.4.3.1
    - fix a crash on startup

--- lua/firetweakdata.lua
+++ lua/firetweakdata.lua
@@ -1,21 +1,21 @@
-Hooks:PostHook(FireTweakData, "_init_dot_entries_fire", "eclipse__init_dot_entries_fire", function(self, fire_entries)
+Hooks:PostHook(FireTweakData, "_init_dot_entries_fire", "eclipse__init_dot_entries_fire", function(self)
 	-- incendiary
-	fire_entries.proj_fire_com.dot_damage = 30
-	fire_entries.proj_fire_com.dot_length = 5
+	self.dot_entries.fire.proj_fire_com.dot_damage = 30
+	self.dot_entries.fire.proj_fire_com.dot_length = 5
 
 	-- flamer dot (might just rework later)
-	fire_entries.weapon_flamethrower_mk2 = {
+	self.dot_entries.fire.weapon_flamethrower_mk2 = {
 		dot_trigger_chance = 50,
 		dot_damage = 7.5,
 		dot_length = 1.1,
 		dot_tick_period = 0.5,
 		dot_trigger_max_distance = false,
 	}
-	fire_entries.ammo_flamethrower_mk2_rare = deep_clone(fire_entries.weapon_flamethrower_mk2)
-	fire_entries.ammo_flamethrower_mk2_welldone = deep_clone(fire_entries.weapon_flamethrower_mk2)
+	self.dot_entries.fire.ammo_flamethrower_mk2_rare = deep_clone(self.dot_entries.fire.weapon_flamethrower_mk2)
+	self.dot_entries.fire.ammo_flamethrower_mk2_welldone = deep_clone(self.dot_entries.fire.weapon_flamethrower_mk2)
 
 	-- db dot (might rework too)
-	fire_entries.ammo_dragons_breath = {
+	self.dot_entries.fire.ammo_dragons_breath = {
 		dot_trigger_chance = 1,
 		dot_damage = 3,
 		dot_length = 4,
