388bb9beaa45b0bab719fb3a0e90b1e3857045c5
mrcreepysos <mrcreepysos@users.noreply.github.com>
2024-01-30 13:44:54 +0000

    Code formatting

--- lua/tweakdatavr.lua
+++ lua/tweakdatavr.lua
@@ -4,31 +4,30 @@ Hooks:PostHook(TweakDataVR, "init", "eclipse_init", function(self)
 end)
 
 -- remove perk deck dash bonuses cause those just trivialize the game half the time
-TweakDataVR.init_specializations = function(self, tweak_data)
-end
+TweakDataVR.init_specializations = function(self, tweak_data) end
 
 -- remove most of the skill tree dash bonuses
 -- mostly cause they either feel unimpactful or are too much, like the vanilla close-by one
 -- keep the sprinter one cause otherwise you get no dodge for movement if you use dashing
 TweakDataVR.init_skills = function(self, tweak_data)
-    self.post_warp = {
-        min = 1,
-        max = 5
-    }
-    self.steelsight_stamina_regen = 0.02
+	self.post_warp = {
+		min = 1,
+		max = 5,
+	}
+	self.steelsight_stamina_regen = 0.02
 
-    if _G.IS_VR then
+	if _G.IS_VR then
 		table.insert(tweak_data.skilltree.skills.optic_illusions[2].upgrades, "player_run_dodge_chance_vr")
-    end
+	end
 
-    local stamina_regen_macro = tostring(self.steelsight_stamina_regen * 100) .. "%"
-    self.skill_descs_addons = {
+	local stamina_regen_macro = tostring(self.steelsight_stamina_regen * 100) .. "%"
+	self.skill_descs_addons = {
 		optic_illusions = {
 			text_id = "menu_vr_addon_sprinter",
 			macros = {
 				min = self.post_warp.min,
-				max = self.post_warp.max
-			}
-		}
+				max = self.post_warp.max,
+			},
+		},
 	}
 end
