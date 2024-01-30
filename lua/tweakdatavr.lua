4d275ec5613276eb3a80a96b11d6b3d666c9b2a8
nikita <59087849+mrcreepysos@users.noreply.github.com>
2024-01-30 16:44:23 +0300

    remove most of vr dash bonuses

--- lua/tweakdatavr.lua
+++ lua/tweakdatavr.lua
@@ -2,3 +2,33 @@
 Hooks:PostHook(TweakDataVR, "init", "eclipse_init", function(self)
 	self.long_range_damage_reduction = { 0, 0 }
 end)
+
+-- remove perk deck dash bonuses cause those just trivialize the game half the time
+TweakDataVR.init_specializations = function(self, tweak_data)
+end
+
+-- remove most of the skill tree dash bonuses
+-- mostly cause they either feel unimpactful or are too much, like the vanilla close-by one
+-- keep the sprinter one cause otherwise you get no dodge for movement if you use dashing
+TweakDataVR.init_skills = function(self, tweak_data)
+    self.post_warp = {
+        min = 1,
+        max = 5
+    }
+    self.steelsight_stamina_regen = 0.02
+
+    if _G.IS_VR then
+		table.insert(tweak_data.skilltree.skills.optic_illusions[2].upgrades, "player_run_dodge_chance_vr")
+    end
+
+    local stamina_regen_macro = tostring(self.steelsight_stamina_regen * 100) .. "%"
+    self.skill_descs_addons = {
+		optic_illusions = {
+			text_id = "menu_vr_addon_sprinter",
+			macros = {
+				min = self.post_warp.min,
+				max = self.post_warp.max
+			}
+		}
+	}
+end
