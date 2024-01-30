c3dcc603d5d672d04ecbef5a23a9713f9f059032
nikita <59087849+mrcreepysos@users.noreply.github.com>
2024-01-30 04:07:32 +0300

    some stealth detection speed stuff

--- lua/playertweakdata.lua
+++ lua/playertweakdata.lua
@@ -1,26 +1,46 @@
 function PlayerTweakData:_set_normal()
 	self.damage.REVIVE_HEALTH_STEPS = { 0.7 }
 	self.damage.MIN_DAMAGE_INTERVAL = 0.4
+
+	self.suspicion.max_value = 8
+	self.suspicion.range_mul = 0.8
+	self.suspicion.buildup_mul = 0.8
 end
 
 function PlayerTweakData:_set_hard()
 	self.damage.REVIVE_HEALTH_STEPS = { 0.6 }
 	self.damage.MIN_DAMAGE_INTERVAL = 0.3
+
+	self.suspicion.max_value = 9
+	self.suspicion.range_mul = 1
+	self.suspicion.buildup_mul = 1
 end
 
 function PlayerTweakData:_set_overkill()
 	self.damage.REVIVE_HEALTH_STEPS = { 0.5 }
 	self.damage.MIN_DAMAGE_INTERVAL = 0.25
+
+	self.suspicion.max_value = 10
+	self.suspicion.range_mul = 1.2
+	self.suspicion.buildup_mul = 1.2
 end
 
 function PlayerTweakData:_set_overkill_145()
 	self.damage.REVIVE_HEALTH_STEPS = { 0.4 }
 	self.damage.MIN_DAMAGE_INTERVAL = 0.2
+
+	self.suspicion.max_value = 11
+	self.suspicion.range_mul = 1.4
+	self.suspicion.buildup_mul = 1.4
 end
 
 function PlayerTweakData:_set_easy_wish()
 	self.damage.REVIVE_HEALTH_STEPS = { 0.4 }
 	self.damage.MIN_DAMAGE_INTERVAL = 0.15
+
+	self.suspicion.max_value = 12
+	self.suspicion.range_mul = 1.7
+	self.suspicion.buildup_mul = 1.7
 end
 
 Hooks:PostHook(PlayerTweakData, "init", "eclipse__init", function(self)
