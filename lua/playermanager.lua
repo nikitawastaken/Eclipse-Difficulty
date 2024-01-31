6831eae1d869ebfcdd20ef27e6bedc4f97cd0b40
nikita <59087849+mrcreepysos@users.noreply.github.com>
2024-01-31 04:36:26 +0300

    reduce damage taken while inside of vehicles
    this is actually a really old one from sh, idk why i never put it before cause you can sometimes get absolutely swiss cheese'ed in the vehicles
    
    Co-Authored-By: Sebastian Gabl (aka Hoppip) <sebastian.gabl@gmx.at>

--- lua/playermanager.lua
+++ lua/playermanager.lua
@@ -208,3 +208,16 @@ function PlayerManager:skill_dodge_chance(...)
 
 	return dodge
 end
+
+-- Reduce damage taken while inside of vehicles
+local damage_reduction_skill_multiplier_original = PlayerManager.damage_reduction_skill_multiplier
+function PlayerManager:damage_reduction_skill_multiplier(...)
+	local dmg_reduction = damage_reduction_skill_multiplier_original(self, ...)
+
+	local player = self:player_unit()
+	if player and player:movement()._current_state_name == "driving" then
+		dmg_reduction = dmg_reduction * 0.33
+	end
+
+	return dmg_reduction
+end
