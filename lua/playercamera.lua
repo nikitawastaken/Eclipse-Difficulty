506950f308a5a3ea4e51ae2a9319cf30e0261680
mrcreepysos <mrcreepysos@users.noreply.github.com>
2024-02-13 19:00:05 +0000

    Code formatting

--- lua/playercamera.lua
+++ lua/playercamera.lua
@@ -4,9 +4,7 @@ function PlayerCamera:play_shaker(effect, amplitude, ...)
 	return play_shaker_original(
 		self,
 		effect,
-		effect == "player_bullet_damage" and self._damage_bullet_shake_multiplier
-			or effect == "player_bullet_damage_knock_out" and self._damage_bullet_shake_multiplier
-			or amplitude,
+		effect == "player_bullet_damage" and self._damage_bullet_shake_multiplier or effect == "player_bullet_damage_knock_out" and self._damage_bullet_shake_multiplier or amplitude,
 		...
 	)
 end
