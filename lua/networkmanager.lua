24b53721fd577fa16a8f3222e01f8c485d055085
nikita <59087849+mrcreepysos@users.noreply.github.com>
2024-02-18 17:56:39 +0300

    give dev a separate mm key
    gone too far with changes, main users now crash when joining dev

--- lua/networkmanager.lua
+++ lua/networkmanager.lua
@@ -1,2 +1,2 @@
-NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY .. "_eclipse_main"
-NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY .. "_eclipse_main"
+NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY .. "_eclipse_dev"
+NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY .. "_eclipse_dev"
