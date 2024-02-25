1a336c39942ddbd625f130ffd1cc93802364e45d
nikita <59087849+mrcreepysos@users.noreply.github.com>
2024-02-26 02:04:25 +0300

    reenable swat turret at the end of buluc
    honestly this is an easier and faster solution, saves me a decent portion of scripting edits to make the escape sequence behave like i would like it to without the turret
    
    turrets are incredibly weak in eclipse anyway so this is fine

--- lua/animatedvehiclebase.lua
+++ lua/animatedvehiclebase.lua
@@ -2,7 +2,7 @@
 -- courtesy of gorg bus
 set_animated_vehicle_base_spawn_original = AnimatedVehicleBase.spawn_module
 local level = Global.level_data and Global.level_data.level_id or ""
-if level ~= "des" and level ~= "chca" and level ~= "friend" then
+if level ~= "des" and level ~= "chca" and level ~= "friend" and level ~= "fex" then
 	function AnimatedVehicleBase:spawn_module(module_unit_name, ...)
 		if type_name(module_unit_name) == "spawn_turret" then
 			return set_animated_vehicle_base_spawn_original(self, module_unit_name, ...)
