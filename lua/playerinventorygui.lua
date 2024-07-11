f4c3dab49851d8b8d04084030910276e873ecb02
nikita <59087849+mrcreepysos@users.noreply.github.com>
2024-07-11 17:16:50 +0300

    add ammo pickup to mouse-over inventory gui

--- lua/playerinventorygui.lua
+++ lua/playerinventorygui.lua
@@ -7,3 +7,57 @@ Hooks:PreHook(PlayerInventoryGui, "set_melee_stats", "shc_set_melee_stats", func
 		end
 	end
 end)
+
+Hooks:PostHook(PlayerInventoryGui, "_update_stats", "eclipse__update_stats", function(self, name)
+	if name == "primary" or name == "secondary" then
+		local stats = {
+			{
+				round_value = true,
+				name = "magazine",
+				stat_name = "extra_ammo"
+			},
+			{
+				round_value = true,
+				name = "totalammo",
+				stat_name = "total_ammo_mod"
+			},
+			{
+				name = "pickup",
+			},
+			{
+				inverted = true,
+				name = "reload",
+			},
+			{
+				round_value = true,
+				name = "fire_rate"
+			},
+			{
+				name = "damage"
+			},
+			{
+				percent = true,
+				name = "spread",
+				offset = true,
+				revert = true
+			},
+			{
+				percent = true,
+				name = "recoil",
+				offset = true,
+				revert = true
+			},
+			{
+				index = true,
+				name = "concealment"
+			},
+			{
+				percent = false,
+				name = "suppression",
+				offset = true
+			}
+		}
+		self:set_weapon_stats(self._info_panel, stats)
+		self:_update_info_weapon(name)
+	end
+end)
