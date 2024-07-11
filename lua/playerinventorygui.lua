12341c0455a9fd8d3381a22258afd12ba33c71f4
mrcreepysos <mrcreepysos@users.noreply.github.com>
2024-07-11 14:17:22 +0000

    Code formatting

--- lua/playerinventorygui.lua
+++ lua/playerinventorygui.lua
@@ -14,12 +14,12 @@ Hooks:PostHook(PlayerInventoryGui, "_update_stats", "eclipse__update_stats", fun
 			{
 				round_value = true,
 				name = "magazine",
-				stat_name = "extra_ammo"
+				stat_name = "extra_ammo",
 			},
 			{
 				round_value = true,
 				name = "totalammo",
-				stat_name = "total_ammo_mod"
+				stat_name = "total_ammo_mod",
 			},
 			{
 				name = "pickup",
@@ -30,32 +30,32 @@ Hooks:PostHook(PlayerInventoryGui, "_update_stats", "eclipse__update_stats", fun
 			},
 			{
 				round_value = true,
-				name = "fire_rate"
+				name = "fire_rate",
 			},
 			{
-				name = "damage"
+				name = "damage",
 			},
 			{
 				percent = true,
 				name = "spread",
 				offset = true,
-				revert = true
+				revert = true,
 			},
 			{
 				percent = true,
 				name = "recoil",
 				offset = true,
-				revert = true
+				revert = true,
 			},
 			{
 				index = true,
-				name = "concealment"
+				name = "concealment",
 			},
 			{
 				percent = false,
 				name = "suppression",
-				offset = true
-			}
+				offset = true,
+			},
 		}
 		self:set_weapon_stats(self._info_panel, stats)
 		self:_update_info_weapon(name)
