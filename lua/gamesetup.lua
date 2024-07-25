b236487e39251d6f3964b41720bd770fae9dabc5
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2024-07-25 21:10:29 +0200

    just in case

--- lua/gamesetup.lua
+++ lua/gamesetup.lua
@@ -1,4 +1,5 @@
 Hooks:PostHook(GameSetup, "load_packages", "sh_load_packages", function(self)
+ local level = Global.level_data and Global.level_data.level_id or ""
 	local difficulty = Global.game_settings and Global.game_settings.difficulty or "normal"
 	local difficulty_index = tweak_data:difficulty_to_index(difficulty)
 	if difficulty_index == 6 and not PackageManager:loaded("packages/sm_wish") then
@@ -10,7 +11,7 @@ Hooks:PostHook(GameSetup, "load_packages", "sh_load_packages", function(self)
 	--For Elite/ZEAL shield's Terminator styled BANG BANG BANG spawn sound
 	if difficulty_index == 6 and not PackageManager:loaded("soundbanks/sfx_hos") then
 		PackageManager:load("soundbanks/sfx_hos")
-	elseif difficulty_index ~= 6 and PackageManager:loaded("soundbanks/sfx_hos") then
+	elseif difficulty_index ~= 6 and level ~= "nmh" and PackageManager:loaded("soundbanks/sfx_hos") then
 		PackageManager:unload("soundbanks/sfx_hos")
 	end
 end)
