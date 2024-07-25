19f711134e63707312624dcc5dfabba486eca7e7
SonicSoapyBoi <SonicSoapyBoi@users.noreply.github.com>
2024-07-25 19:10:55 +0000

    Code formatting

--- lua/gamesetup.lua
+++ lua/gamesetup.lua
@@ -1,5 +1,5 @@
 Hooks:PostHook(GameSetup, "load_packages", "sh_load_packages", function(self)
- local level = Global.level_data and Global.level_data.level_id or ""
+	local level = Global.level_data and Global.level_data.level_id or ""
 	local difficulty = Global.game_settings and Global.game_settings.difficulty or "normal"
 	local difficulty_index = tweak_data:difficulty_to_index(difficulty)
 	if difficulty_index == 6 and not PackageManager:loaded("packages/sm_wish") then
