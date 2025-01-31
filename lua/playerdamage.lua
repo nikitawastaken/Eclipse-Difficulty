e5515e4d15deced80de1aedda314eb29432ddfed
hitscanner <1my3mail3@gmail.com>
2025-01-31 16:09:21 +0100

    Cloakers no longer subtract downs

--- lua/playerdamage.lua
+++ lua/playerdamage.lua
@@ -12,6 +12,15 @@ Hooks:PreHook(PlayerDamage, "replenish", "eclipse_replenish", function(self)
 	end
 end)
 
+-- armor regen time depends on the armor you're wearing
+function PlayerDamage:set_regenerate_timer_to_max()
+	local mul = managers.player:body_armor_regen_multiplier(alive(self._unit) and self._unit:movement():current_state()._moving, self:health_ratio())
+	self._regenerate_timer = managers.player:body_armor_value("regen_timer") * mul
+	self._regenerate_timer = self._regenerate_timer * managers.player:upgrade_value("player", "armor_regen_time_mul", 1)
+	self._regenerate_speed = self._regenerate_speed or 1
+	self._current_state = self._update_regenerate_timer
+end
+
 -- Remove suppression
 function PlayerDamage:_upd_suppression(t, dt) end
 
@@ -188,121 +197,51 @@ function PlayerDamage:damage_bullet(attack_data)
 	self:_call_listeners(damage_info)
 end
 
--- Friendly Fire
-function PlayerDamage:is_friendly_fire(unit)
-	local attacker_mov_ext = alive(unit) and unit:movement()
-
-	if not attacker_mov_ext or not attacker_mov_ext.team or not attacker_mov_ext.friendly_fire then
-		return false
-	end
-
-	local my_team = self._unit:movement():team()
-	local attacker_team = attacker_mov_ext:team()
-
-	if attacker_team ~= my_team and attacker_mov_ext:friendly_fire() then
-		return false
-	end
-	local pro_job_enabled = Global.game_settings and Global.game_settings.one_down
-	local attacked_by_foe = attacker_team and my_team and my_team.foes[attacker_team.id]
-	local friendly_fire_mutator_active = managers.mutators:modify_value("PlayerDamage:FriendlyFire", friendly_fire_mutator_active) == false
-	if not attacked_by_foe then
-		if pro_job_enabled or friendly_fire_mutator_active then
-			return false
-		end
-		return true
-	end
-	return false
-end
-
--- Armor Breaking GP / Panic
-function PlayerDamage:_calc_armor_damage(attack_data)
-	local health_subtracted = 0
-
-	if self:get_real_armor() > 0 then
-		health_subtracted = self:get_real_armor()
-
-		self:change_armor(-attack_data.damage)
-
-		health_subtracted = health_subtracted - self:get_real_armor()
-
-		self:_damage_screen()
-		SoundDevice:set_rtpc("shield_status", self:armor_ratio() * 100)
-		self:_send_set_armor()
-
-		local has_armor_panic = managers.player:has_enabled_cooldown_upgrade("cooldown", "panic_on_armor_break")
-
-		if self:get_real_armor() <= 0 then
-			-- Armor breaking causes enemies to panic
-			if has_armor_panic then
-				local pos = managers.player:player_unit():position()
-				local skill = tweak_data.upgrades.values.player.armor_panic[1]
-
-				if skill then
-					local area = skill.area
-					local chance = skill.chance
-					local amount = skill.amount
-					local enemies = World:find_units_quick("sphere", pos, area, managers.slot:get_mask("enemies"))
-
-					for i, unit in ipairs(enemies) do
-						if unit:character_damage() then
-							unit:character_damage():build_suppression(amount, chance)
-						end
-					end
-				end
-
-				managers.player:disable_cooldown_upgrade("cooldown", "panic_on_armor_break")
-			end
-
-			self._unit:sound():play("player_armor_gone_stinger")
-
-			if attack_data.armor_piercing then
-				self._unit:sound():play("player_sniper_hit_armor_gone")
-			end
-
-			-- Add significantly longer grace period on armor break (repurposing Anarchist/Armorer damage timer) (sh)
-			self._can_take_dmg_timer = math.max(self._dmg_interval, 0.3)
-
-			local pm = managers.player
-
-			self:_start_regen_on_the_side(pm:upgrade_value("player", "passive_always_regen_armor", 0))
-
-			if pm:has_inactivate_temporary_upgrade("temporary", "armor_break_invulnerable") then
-				pm:activate_temporary_upgrade("temporary", "armor_break_invulnerable")
-
-				self._can_take_dmg_timer = pm:temporary_upgrade_value("temporary", "armor_break_invulnerable", 0)
-			end
-		end
-	end
-
-	managers.hud:damage_taken()
-
-	return health_subtracted
-end
-
 -- Grace period protects no matter the new potential damage but is shorter in general (sh)
 function PlayerDamage:_chk_dmg_too_soon()
 	local next_allowed_dmg_t = type(self._next_allowed_dmg_t) == "number" and self._next_allowed_dmg_t or Application:digest_value(self._next_allowed_dmg_t, false)
 	return managers.player:player_timer():time() < next_allowed_dmg_t
 end
 
+-- Add slightly longer grace period on armor break (repurposing Anarchist/Armorer damage timer) / Add a skill that causes nearby enemies to panic when armor is broken
+local _calc_armor_damage_original = PlayerDamage._calc_armor_damage
+function PlayerDamage:_calc_armor_damage(...)
+	local had_armor = self:get_real_armor() > 0
+	
+	local health_subtracted = _calc_armor_damage_original(self, ...)
+	
+	local has_armor_panic = managers.player:has_enabled_cooldown_upgrade("cooldown", "panic_on_armor_break")	
+	
+	if had_armor and self:get_real_armor() <= 0 then
+		if has_armor_panic then
+			local pos = managers.player:player_unit():position()
+			local skill = tweak_data.upgrades.values.player.armor_panic[1]
+
+			if skill then
+				local area = skill.area
+				local chance = skill.chance
+				local amount = skill.amount
+				local enemies = World:find_units_quick("sphere", pos, area, managers.slot:get_mask("enemies"))
+
+				for i, unit in ipairs(enemies) do
+					if unit:character_damage() then
+						unit:character_damage():build_suppression(amount, chance)
+					end
+				end
+			end
+
+			managers.player:disable_cooldown_upgrade("cooldown", "panic_on_armor_break")
+		end		
+		
+		if health_subtracted > 0 and self._can_take_dmg_timer <= 0 then
+			self._can_take_dmg_timer = self._dmg_interval + (tweak_data.player.damage.ARMOR_BREAK_MIN_DAMAGE_INTERVAL or 0.15)
+		end
+	end
+	
+	return health_subtracted
+end
+
 function PlayerDamage:_calc_health_damage(attack_data)
-	-- damage tagging, (IT WAS NOT) worth the experiment i think
-	-- if attack_data.weapon_unit and attack_data.damage > 15 then
-	-- 	local armor_value_tagged = managers.player:body_armor_value("damage_tagged")
-	-- 	local skill_value_tagged = managers.player:upgrade_value("player", "player_tagged_speed_mul", 1)
-	-- 	local slowdown_data = {
-	-- 		max_mul = math.clamp(0.2 * armor_value_tagged * skill_value_tagged, 0, 1),
-	-- 		add_mul = math.clamp(math.min(0.07, 0.0015 * attack_data.damage) / armor_value_tagged * skill_value_tagged, 0, 1),
-	-- 		decay_time = math.min(1.5, 0.01 * attack_data.damage),
-	-- 		id = "snowthrower_cold",
-	-- 		duration = 2,
-	-- 		mul = math.clamp(math.min(0.7, 60 / attack_data.damage) * armor_value_tagged / skill_value_tagged, 0, 1),
-	-- 		prevents_running = false,
-	-- 	}
-
-	-- 	self:apply_slowdown(slowdown_data)
-	-- end
-
 	if managers.player:has_activate_temporary_upgrade("temporary", "mrwi_health_invulnerable") then
 		return 0
 	end
@@ -360,17 +299,27 @@ function PlayerDamage:_calc_health_damage(attack_data)
 	return health_subtracted
 end
 
-function PlayerDamage:on_incapacitated()
-	local is_pro = Global.game_settings and Global.game_settings.one_down
+-- add an upgrade that gives increased bleedout timer
+Hooks:PostHook(PlayerDamage, "_regenerated", "sh__regenerated", function (self)
+    self._down_time_i = 0
+	self._down_time = tweak_data.player.damage.DOWNED_TIME + managers.player:upgrade_value("player", "increased_bleedout_timer", 0)
+end)
 
-	self:on_downed()
+--Fix fake downs progressing revive health and down timer steps
+local revive_original = PlayerDamage.revive
+function PlayerDamage:revive(...)
+    local was_bleedout = self._bleed_out
 
-	if is_pro then
-		self._revives = Application:digest_value(Application:digest_value(self._revives, false) - 1, true) -- instant incaps (cloakers / tasers) count as downs
-		self:_send_set_revives()
-	end
+    revive_original(self, ...)
 
-	self._incapacitated = true
+    if was_bleedout then
+        self._down_time_i = self._down_time_i + 1
+    else
+        self._revive_health_i = math.max(self._revive_health_i - 1, 1)
+    end
+
+    local player_damage_tweak = tweak_data.player.damage
+    self._down_time = math.max(player_damage_tweak.DOWNED_TIME_MIN, player_damage_tweak.DOWNED_TIME - player_damage_tweak.DOWNED_TIME_DEC * self._down_time_i)
 end
 
 -- make healing fixed instead of % of max health
@@ -388,20 +337,6 @@ function PlayerDamage:on_copr_killshot()
 	self._last_received_dmg = self:_max_health()
 end
 
--- add an upgrade that gives increased bleedout timer
-Hooks:PostHook(PlayerDamage, "_regenerated", "eclipse__regenerated", function(self)
-	self._down_time = tweak_data.player.damage.DOWNED_TIME + managers.player:upgrade_value("player", "increased_bleedout_timer", 0)
-
-	self._down_timer_max = self._down_time -- store the max bleedout timer so that fak down restore can go up to 35
-end)
-
--- bring back decreasing bleedout timer based on the amount of downs
-Hooks:PreHook(PlayerDamage, "revive", "eclipse_revive", function(self)
-	if not self:arrested() then
-		self._down_time = math.max(tweak_data.player.damage.DOWNED_TIME_MIN, self._down_time - tweak_data.player.damage.DOWNED_TIME_DEC)
-	end
-end)
-
 -- faks only heal a small portion but then heal you over time
 function PlayerDamage:band_aid_health(hot_regen)
 	if managers.platform:presence() == "Playing" and (self:arrested() or self:need_revive()) then
@@ -417,17 +352,34 @@ function PlayerDamage:band_aid_health(hot_regen)
 end
 
 -- Fix Anarchist regen not triggering HUD armor update for clients
-Hooks:PostHook(PlayerDamage, "change_armor", "sh_change_armor", function(self, change)
+Hooks:PostHook(PlayerDamage, "change_armor", "eclipse_change_armor", function(self, change)
 	if change > 0 and self:armor_ratio() < 1 then
 		self:_send_set_armor()
 	end
 end)
 
--- armor regen time depends on the armor you're wearing
-function PlayerDamage:set_regenerate_timer_to_max()
-	local mul = managers.player:body_armor_regen_multiplier(alive(self._unit) and self._unit:movement():current_state()._moving, self:health_ratio())
-	self._regenerate_timer = managers.player:body_armor_value("regen_timer") * mul
-	self._regenerate_timer = self._regenerate_timer * managers.player:upgrade_value("player", "armor_regen_time_mul", 1)
-	self._regenerate_speed = self._regenerate_speed or 1
-	self._current_state = self._update_regenerate_timer
+-- Friendly Fire
+function PlayerDamage:is_friendly_fire(unit)
+	local attacker_mov_ext = alive(unit) and unit:movement()
+
+	if not attacker_mov_ext or not attacker_mov_ext.team or not attacker_mov_ext.friendly_fire then
+		return false
+	end
+
+	local my_team = self._unit:movement():team()
+	local attacker_team = attacker_mov_ext:team()
+
+	if attacker_team ~= my_team and attacker_mov_ext:friendly_fire() then
+		return false
+	end
+	local pro_job_enabled = Global.game_settings and Global.game_settings.one_down
+	local attacked_by_foe = attacker_team and my_team and my_team.foes[attacker_team.id]
+	local friendly_fire_mutator_active = managers.mutators:modify_value("PlayerDamage:FriendlyFire", friendly_fire_mutator_active) == false
+	if not attacked_by_foe then
+		if pro_job_enabled or friendly_fire_mutator_active then
+			return false
+		end
+		return true
+	end
+	return false
 end
