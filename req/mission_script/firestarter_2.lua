9f30829615dd5a31cb98c525f006f1154f3092bf
hitscanner <1my3mail3@gmail.com>
2025-02-05 21:08:38 +0100

    Mission script part 2

--- req/mission_script/firestarter_2.lua
+++ req/mission_script/firestarter_2.lua
@@ -4,5 +4,26 @@ return {
 			length = 60,
 			player_mul = { 2.5, 1.5, 1, 1 }
 		}
-	}
+	},
+	[100962] = {
+		values = {
+			interval = 15,
+		},
+		groups = {
+			tac_shield_wall = false,
+			tac_shield_wall_ranged = false,
+			tac_shield_wall_charge = false,
+			tac_bull_rush = false,
+		},
+	},
+	[100963] = {
+		values = {
+			interval = 20,
+		},
+	},
+	[100961] = {
+		values = {
+			interval = 30,
+		},
+	},
 }
