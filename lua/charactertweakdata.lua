d17df3d2da58ceb301317144ddd7d3a987f0464b
hitscanner <1my3mail3@gmail.com>
2025-02-05 19:13:10 +0100

    Nerf Team AI HP

--- lua/charactertweakdata.lua
+++ lua/charactertweakdata.lua
@@ -448,7 +448,7 @@ function CharacterTweakData:_presets(tweak_data, ...)
 	speed_multiplier(presets.move_speed.very_fast, 1.3)
 	speed_multiplier(presets.move_speed.lightning, 1.4)
 
-	presets.gang_member_damage.HEALTH_INIT = 100 + (50 * diff_i)
+	presets.gang_member_damage.HEALTH_INIT = 60 + (30 * diff_i)
 	presets.gang_member_damage.HEALTH_REGEN = presets.gang_member_damage.HEALTH_INIT * 0.15
 	presets.gang_member_damage.MIN_DAMAGE_INTERVAL = 0.05
 	presets.gang_member_damage.REGENERATE_TIME = 5
