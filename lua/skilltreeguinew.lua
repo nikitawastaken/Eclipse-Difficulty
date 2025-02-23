1ae16cd18cf170de2a3469175193bd9eb2dc1052
nikitawastaken <nikitawastaken@users.noreply.github.com>
2025-02-23 14:58:52 +0000

    Code formatting

--- lua/skilltreeguinew.lua
+++ lua/skilltreeguinew.lua
@@ -18,7 +18,7 @@ function NewSkillTreeGui:setbgimg(page, init)
 		self._bg_image5 = nil
 
 		self._fullscreen_ws = managers.gui_data:create_fullscreen_workspace()
-		self._fullscreen_panel = self._fullscreen_ws:panel():panel({name = "fullscreen"})
+		self._fullscreen_panel = self._fullscreen_ws:panel():panel({ name = "fullscreen" })
 
 		self._bg_image1 = self._fullscreen_panel:bitmap({
 			name = "bg_image1",
@@ -26,7 +26,7 @@ function NewSkillTreeGui:setbgimg(page, init)
 			w = self._fullscreen_panel:w(),
 			h = self._fullscreen_panel:h(),
 			layer = 1,
-			blend_mode = "add"
+			blend_mode = "add",
 		})
 
 		self._bg_image2 = self._fullscreen_panel:bitmap({
@@ -35,7 +35,7 @@ function NewSkillTreeGui:setbgimg(page, init)
 			w = self._fullscreen_panel:w(),
 			h = self._fullscreen_panel:h(),
 			layer = 1,
-			blend_mode = "add"
+			blend_mode = "add",
 		})
 
 		self._bg_image3 = self._fullscreen_panel:bitmap({
@@ -44,7 +44,7 @@ function NewSkillTreeGui:setbgimg(page, init)
 			w = self._fullscreen_panel:w(),
 			h = self._fullscreen_panel:h(),
 			layer = 1,
-			blend_mode = "add"
+			blend_mode = "add",
 		})
 
 		self._bg_image4 = self._fullscreen_panel:bitmap({
@@ -53,7 +53,7 @@ function NewSkillTreeGui:setbgimg(page, init)
 			w = self._fullscreen_panel:w(),
 			h = self._fullscreen_panel:h(),
 			layer = 1,
-			blend_mode = "add"
+			blend_mode = "add",
 		})
 
 		self._bg_image5 = self._fullscreen_panel:bitmap({
@@ -62,10 +62,9 @@ function NewSkillTreeGui:setbgimg(page, init)
 			w = self._fullscreen_panel:w(),
 			h = self._fullscreen_panel:h(),
 			layer = 1,
-			blend_mode = "add"
+			blend_mode = "add",
 		})
 	end
-
 
 	for i, panel in ipairs(bgpanels) do
 		self[panel]:set_alpha(image_transparency)
@@ -73,16 +72,16 @@ function NewSkillTreeGui:setbgimg(page, init)
 		local texture_width = self[panel]:texture_width()
 		local texture_height = self[panel]:texture_height()
 
-	if adjust_aspect_ratio == true then
-		local correct_height = self._fullscreen_panel:w() / (16/9) -- actual menu aspect ratio
-		self[panel]:set_size(correct_height, correct_height)
-	else
-		local sw = math.max(texture_width, texture_height * aspect)
-		local sh = math.max(texture_height, texture_width / aspect)
-		local dw = texture_width / sw
-		local dh = texture_height / sh
-		self[panel]:set_size(dw * self._fullscreen_panel:w(), dh * self._fullscreen_panel:h())
-	end
+		if adjust_aspect_ratio == true then
+			local correct_height = self._fullscreen_panel:w() / (16 / 9) -- actual menu aspect ratio
+			self[panel]:set_size(correct_height, correct_height)
+		else
+			local sw = math.max(texture_width, texture_height * aspect)
+			local sh = math.max(texture_height, texture_width / aspect)
+			local dw = texture_width / sw
+			local dh = texture_height / sh
+			self[panel]:set_size(dw * self._fullscreen_panel:w(), dh * self._fullscreen_panel:h())
+		end
 
 		self[panel]:set_right(self._fullscreen_panel:w())
 		self[panel]:set_center_y(self._fullscreen_panel:h() / 2)
@@ -105,14 +104,14 @@ function NewSkillTreeGui:setbgimg(page, init)
 	end
 end
 
-Hooks:PreHook( NewSkillTreeGui , "init" , "gibskillbg_init" , function( self , params )
+Hooks:PreHook(NewSkillTreeGui, "init", "gibskillbg_init", function(self, params)
 	NewSkillTreeGui:setbgimg(1, true)
 end)
 
-Hooks:PostHook( NewSkillTreeGui , "set_active_page" , "gibskillbg_setpage" , function( self , params )
+Hooks:PostHook(NewSkillTreeGui, "set_active_page", "gibskillbg_setpage", function(self, params)
 	NewSkillTreeGui:setbgimg(self._active_page, false)
 end)
 
-Hooks:PostHook( NewSkillTreeGui , "close" , "gibtest2" , function( self , params )
+Hooks:PostHook(NewSkillTreeGui, "close", "gibtest2", function(self, params)
 	NewSkillTreeGui:setbgimg(0, false)
 end)
