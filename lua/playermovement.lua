a8d228b0ad6a67e7c9f7a6f1539dc193cecc436e
nikitawastaken <59087849+nikitawastaken@users.noreply.github.com>
2025-03-04 17:50:10 +0300

    edit the aimpunch and cloakerpunch effects
    main aimpunch is just more sudden, the strength remains the same
    cloaker punch effect is now more resembling of the old "massive knockout" one but randomized, instead of the "woozy spin" effect

--- lua/playermovement.lua
+++ lua/playermovement.lua
@@ -34,7 +34,7 @@ function PlayerMovement:on_SPOOCed(enemy_unit)
 
 			self._unit:character_damage():change_health(-spooc_kick_damage)
 
-			local effect = "melee_hit_spooc_var" .. math.random(1, 4)
+			local effect = "melee_hit_spooc_var" .. math.random(1, 2)
 
 			self._unit:camera():play_shaker(effect, 1)
 		else
