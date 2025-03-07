ccaa2fe1cd688294883707e13cfa7e5278c36c13
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2025-03-07 12:55:24 +0100

    restore firestarter day 1 snipers

--- req/mission_script/framing_frame_3.lua
+++ req/mission_script/framing_frame_3.lua
@@ -9,6 +9,18 @@ return {
 			base_delay_rand = 15,
 		},
 	},
+    --Spawn snipers after 120 seconds of starting the assault
+	[103812] = { 
+		on_executed = {
+			{id = 400010, delay = 120}
+		}
+	},
+	--this makes snipers in the nearby building not floating
+	[100318] = { 
+		on_executed = {
+			{id = 105543, delay = 1}
+		}
+	},
 	-- prevent shields and dozers from disabling the power
 	[104699] = no_shields_and_dozers,
 	[104700] = no_shields_and_dozers,
