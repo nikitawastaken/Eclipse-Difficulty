a4d90ba5a381775fe02520ee552cd3a84dc98834
hitscanner <1my3mail3@gmail.com>
2025-03-16 21:49:19 +0100

    Shacklethorne and Brooklyn Bank edits

--- req/mission_script/brb.lua
+++ req/mission_script/brb.lua
@@ -1,7 +1,21 @@
-local interval = {
+local preferred = Eclipse.preferred
+local construction_spawn = {
+	values = {
+		interval = 20,
+	},
+	groups = preferred.no_bulldozers,
+}
+local bridge_spawn = {
+	values = {
+		interval = 25,
+	},
+	groups = preferred.no_cops_agents,
+}
+local window_spawn = {
 	values = {
 		interval = 45,
 	},
+	groups = preferred.no_cops_agents_shields_bulldozers,
 }
 return {
 	-- Combine some navigation areas
@@ -14,14 +28,37 @@ return {
 		},
 	},
 	--Don't trigger the spawngroup if the tarp has been cut (should prevent cops from spawning early)
-	--yes, this makes the cops spawn early
+	--Yes, this makes the cops spawn early
 	[101288] = {
 		values = {
 			enabled = false,
 		},
 	},
-	-- Slow down inside spawns
-	[100247] = interval,
-	[100067] = interval,
-	[100068] = interval,
+	-- Add early reinforce around the bank 
+	[100001] = {
+		reinforce = {
+			{
+				name = "parking_lot",
+				force = 3,
+				position = Vector3(-1950, -2750, 10),
+			},
+			{
+				name = "construction",
+				force = 3,
+				position = Vector3(3100, -3750, 10),
+			},
+		},
+	},
+	-- spawn point delays
+	[100461] = construction_spawn,	
+	[100168] = bridge_spawn,
+	[100369] = bridge_spawn,
+	[100429] = bridge_spawn,
+	[100435] = bridge_spawn,
+	[100441] = bridge_spawn,
+	[100454] = bridge_spawn,
+	[100455] = bridge_spawn,	
+	[100247] = window_spawn,
+	[100067] = window_spawn,
+	[100068] = window_spawn,
 }
