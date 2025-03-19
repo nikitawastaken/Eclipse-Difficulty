b875009ef8cfccf7c7dc554d323166677b89daf0
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2025-03-19 18:41:28 +0100

    fix duplicate Rikers

--- req/mission_script/hox_2.lua
+++ req/mission_script/hox_2.lua
@@ -199,6 +199,11 @@ return {
 			{ delay = 0, id = 101198 },
 		},
 	},
+    [101212] = {
+		on_executed = {
+			{ remove = true, id = 101198 },
+		},
+	},
 	-- Exit Bulldozer
 	[102218] = {
 		values = {
