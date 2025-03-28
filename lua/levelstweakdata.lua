4569b6b1164d754b87d8f7adcb529584a2591a44
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2025-03-28 14:13:58 +0100

    Fix a typo

--- lua/levelstweakdata.lua
+++ lua/levelstweakdata.lua
@@ -307,18 +307,18 @@ Hooks:PostHook(LevelsTweakData, "init", "eclipse_init", function(self)
 		},
 	}
 
-	self.chca.ai_unit_group_overrides = deep_clone(self.chca.ai_unit_group_overrides)
-	self.chca.ai_unit_group_overrides.Marshal_gunner_1 = {
+	self.deep.ai_unit_group_overrides = deep_clone(self.chca.ai_unit_group_overrides)
+	self.deep.ai_unit_group_overrides.Marshal_gunner_1 = {
 		america = {
 			Idstring("units/pd2_dlc_usm2/characters/ene_male_marshal_gunner_hcar_2/ene_male_marshal_gunner_hcar_2"),
 		},
 	}
-	self.chca.ai_unit_group_overrides.Marshal_gunner_2 = {
+	self.deep.ai_unit_group_overrides.Marshal_gunner_2 = {
 		america = {
 			Idstring("units/pd2_dlc_usm2/characters/ene_male_marshal_gunner_sko12_2/ene_male_marshal_gunner_sko12_2"),
 		},
 	}
-	self.chca.ai_unit_group_overrides.Marshal_gunner = {
+	self.deep.ai_unit_group_overrides.Marshal_gunner = {
 		america = {
 			Idstring("units/pd2_dlc_usm2/characters/ene_male_marshal_gunner_hcar_2/ene_male_marshal_gunner_hcar_2"),
 			Idstring("units/pd2_dlc_usm2/characters/ene_male_marshal_gunner_sko12_2/ene_male_marshal_gunner_sko12_2"),
