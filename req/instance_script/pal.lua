488220fae6cfc4f4275c8c2b35d550b95b1f959c
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2025-04-05 19:09:11 +0200

    Instance tweaks

---
+++ req/instance_script/pal.lua
@@ -0,0 +1,72 @@
+---@module Counterfeit
+local M = {}
+local scripted_enemy = Eclipse.scripted_enemy
+local heavy_swat = scripted_enemy.heavy_swat_2
+local cloaker = scripted_enemy.cloaker
+local filter_disable = Eclipse.utils.set_diff_groups("disable")
+local filter_normal_above = Eclipse.utils.set_diff_groups("normal_above")
+local filter_eclipse_only = Eclipse.utils.set_diff_groups("eclipse")
+
+local patches = {
+	sewer_spawn = {
+		spooc = table.set(100010),
+		swat_sg = table.set(100019),
+		filters_disable = table.set(100011, 100012),
+		filters_normal_above = table.set(100013),
+		spawn_chance = table.set(100003),
+		squad = table.set(100003),
+	},
+	pipe_spawn = {
+		filters_disable = table.set(100014, 100015),
+		filters_eclipse = table.set(100013),
+		spawn_chance = table.set(100000),
+	},
+}
+
+M["levels/instances/unique/sub_sewer_grate/world/world"] = function(result)
+	local sewer_grate = patches.sewer_spawn
+
+	for _, element in pairs(result.default.elements) do
+		local id = element.id
+
+		if sewer_grate.spooc[id] then
+			element.values.enemy = cloaker
+		elseif sewer_grate.swat_sg[id] then
+			element.values.enemy = heavy_swat
+		elseif sewer_grate.filters_normal_above[id] then
+			table.map_append(element.values, filters_normal_above)
+			element.values.on_executed = {
+					{ id = 100018, remove = true },
+					{ id = 100010, delay = 0, },
+					{ id = 100019, delay = 0, },
+					{ id = 100021, delay = 0, },
+				}
+		elseif sewer_grate.filters_disable[id] then
+			table.map_append(element.values, filters_disable)
+		elseif sewer_grate.spawn_chance[id] then
+			element.values.chance = 45
+		end	
+	end
+end
+
+M["levels/instances/unique/sub_sewer_sidespawn/world/world"] = function(result)
+	local side_spawn = patches.pipe_spawn
+
+	for _, element in pairs(result.default.elements) do
+		local id = element.id
+
+		if side_spawn.filters_eclipse[id] then
+			table.map_append(element.values, filter_eclipse_only)
+		elseif side_spawn.filters_disable[id] then
+			table.map_append(element.values, filters_disable)
+		elseif side_spawn.spawn_chance[id] then
+			element.values.chance = 30
+			element.values.on_executed = {
+					{ id = 100016, remove = true },
+					{ id = 100010, delay = 0, },
+				}
+		end	
+	end
+end
+
+return M
