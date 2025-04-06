ffd4454c072e66d5f87d105ac3050cdcd50a5635
rockymoto517 <rockymoto517@gmail.com>
2025-04-05 20:30:58 -0500

    verify rpc instead of peer_id

--- req/networking.lua
+++ req/networking.lua
@@ -1,6 +1,7 @@
 NetworkHelper:AddReceiveHook("eclipse_hostage_trade", "eclipse_hostage_trade_hook", function(data, sender)
 	local params = json.decode(data)
-	if not BaseNetworkHandler._verify_character_and_sender(params.unit, sender) then
+	local rpc = managers.network:session():peer(sender):rpc()
+	if not BaseNetworkHandler._verify_character_and_sender(params.unit, rpc) then
 		return
 	end
 	CopLogicTrade.hostage_trade(params.unit, params.enable, params.trade_success, params.skip_hint, params.is_custody_trade)
