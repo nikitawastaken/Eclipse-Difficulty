c5cb983f527a187264510d909be2fa8f724cfb80
rockymoto517 <rockymoto517@gmail.com>
2025-04-05 20:58:11 -0500

    test

--- req/networking.lua
+++ req/networking.lua
@@ -1,8 +1,22 @@
+local function get_unit_from_id(unit_id)
+	for _, data in pairs(managers.enemy:all_enemies()) do
+		if unit_id == data:id() then
+			return data
+		end
+	end
+	for _, data in pairs(managers.enemy:all_civilians()) do
+		if unit_id == data:id() then
+			return data
+		end
+	end
+end
+
 NetworkHelper:AddReceiveHook("eclipse_hostage_trade", "eclipse_hostage_trade_hook", function(data, sender)
 	local params = json.decode(data)
-	local rpc = managers.network:session():peer(sender):rpc()
-	if not BaseNetworkHandler._verify_character_and_sender(params.unit, rpc) then
+	local unit = get_unit_from_id(params.unit_id)
+	if not BaseNetworkHandler._verify_character(params.unit) then
 		return
 	end
-	CopLogicTrade.hostage_trade(params.unit, params.enable, params.trade_success, params.skip_hint, params.is_custody_trade)
+	Eclipse:log_chat("called hostage_trade", sender)
+	CopLogicTrade.hostage_trade(unit, params.enable, params.trade_success, params.skip_hint, params.is_custody_trade)
 end)
