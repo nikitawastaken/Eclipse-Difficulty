322590f5c4cd5e74d2b8104c16df3408725a92c0
rockymoto517 <rockymoto517@gmail.com>
2025-04-05 19:21:08 -0500

    forgor

---
+++ req/networking.lua
@@ -0,0 +1,5 @@
+NetworkHelper:AddReceiveHook("eclipse_hostage_trade", "eclipse_hostage_trade_hook", function(data, sender)
+	if not BaseNetworkHandler._verify_character_and_sender(data.unit, sender) then
+		CopLogicTrade.hostage_trade(data.unit, data.enable, data.trade_success, data.skip_hint, data.is_custody_trade)
+	end
+end)
