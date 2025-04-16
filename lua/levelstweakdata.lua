f0083823cb90331eb3685ff95c0de140abc8677a
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2025-04-16 15:27:25 +0200

    fix mendozas being silent in rats day 3

--- lua/levelstweakdata.lua
+++ lua/levelstweakdata.lua
@@ -40,6 +40,10 @@ Hooks:PostHook(LevelsTweakData, "init", "eclipse_init", function(self)
 		"packages/narr_jungle1_night",
 		"levels/narratives/h_alex_must_die/stage_2/world_sounds",
 	}
+	self.alex_3.package = {
+		"packages/narr_alex3",
+		"levels/narratives/h_alex_must_die/stage_1/world_sounds",
+	}
 	self.nightclub.package = {
 		"packages/vlad_nightclub",
 		"levels/narratives/dentist/mia/stage2/world_sounds",
