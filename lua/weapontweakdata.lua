9ec55334ff0c0e103e1d379949076f415110e4ff
hitscanner <1my3mail3@gmail.com>
2025-04-19 10:13:54 +0200

    Experimental recoil adjustments
    -SMGs, ARs, LMGs and Pistols had their recoil reduced by roughly 25%
    -DMRs had their hipfire spread increased by roughly 35%
    -Recoil pattern no longer changes in semi-auto

--- lua/weapontweakdata.lua
+++ lua/weapontweakdata.lua
@@ -130,26 +130,26 @@ function WeaponTweakData:_init_weapons()
 
 					weap_data.spread_multiplier = {
 						standing = {
-							hipfire = 1.25,
+							hipfire = 1.5,
 							crouching = 0.75,
 							steelsight = 0.5,
 						},
 						moving = {
-							hipfire = 1.5,
+							hipfire = 2,
 							crouching = 1,
-							steelsight = 0.875,
+							steelsight = 1.5,
 						}
 					}
 					weap_data.recoil_multiplier = {
 						standing = {
-							hipfire = 1.25,
+							hipfire = 1.5,
 							crouching = 1,
-							steelsight = 1,
+							steelsight = 1.25,
 						},
 						moving = {
-							hipfire = 1.35,
+							hipfire = 2,
 							crouching = 1,
-							steelsight = 1.1,
+							steelsight = 1.5,
 						}
 					}
 				else
@@ -674,38 +674,25 @@ function WeaponTweakData:_init_weapons()
 
 			if weap_data.kick then
 				if cat_map.assault_rifle then
-					weap_data.kick.standing = { 0.8, 1, -0.4, 0.4 }
-					weap_data.kick.single = {}
-					weap_data.kick_burst = {}
-					weap_data.kick.single.standing = { 1, 1.5, -0.2, 0.1 }
+					weap_data.kick.standing = { 0.6, 0.8, -0.4, 0.4 }
 
 				elseif cat_map.smg then
-					weap_data.kick.standing = { 0.6, 0.8, -0.6, 0.6 }
-					weap_data.kick.single = {}
-					weap_data.kick_burst = {}
-					weap_data.kick.single.standing = { 0.8, 1, -0.2, 0.1 }
+					weap_data.kick.standing = { 0.4, 0.6, -0.6, 0.6 }
 
 				elseif cat_map.lmg then
-					weap_data.kick.standing = { 0.4, 0.6, -0.8, 0.8 }
-					weap_data.kick.single = {}
-					weap_data.kick.single.standing = { 0.6, 0.8, -0.2, 0.1 }
+					weap_data.kick.standing = { 0.3, 0.5, -0.8, 0.8 }
 
 				elseif cat_map.minigun then
 					weap_data.kick.standing = { 0.2, 0.4, -0.4, 0.4 }
 
 				elseif cat_map.pistol  then
-					weap_data.kick.standing =  { 1, 1.5, -0.2, 0.1 }
-					weap_data.kick.auto = {}
-					weap_data.kick_burst = {}
-					weap_data.kick.auto.standing = { 0.6, 0.8, -0.6, 0.6 }
+					weap_data.kick.standing =  { 0.8, 1, -0.2, 0.1 }
 
 				elseif cat_map.revolver or cat_map.handcannon then
 					weap_data.kick.standing = { 1.5, 2, -0.4, 0.3 }
 
 				elseif cat_map.shotgun or cat_map.grenade_launcher or cat_map.snp then
 					weap_data.kick.standing = { 2, 3, -0.5, 0.4 }
-					weap_data.kick.auto = {}
-					weap_data.kick.auto.standing = { 1.5, 2, -0.5, 0.5 }
 
 				else
 					weap_data.kick.standing = { 0, 0, 0, 0 }
@@ -713,19 +700,10 @@ function WeaponTweakData:_init_weapons()
 
 				weap_data.kick.crouching = weap_data.kick.standing
 				weap_data.kick.steelsight = weap_data.kick.standing
-
-				if weap_data.kick.single and weap_data.kick.single.standing then
-					weap_data.kick.single.crouching = weap_data.kick.single.standing
-					weap_data.kick.single.steelsight = weap_data.kick.single.standing
-				end
-
+				
 				if weap_data.kick.auto and weap_data.kick.auto.standing then
 					weap_data.kick.auto.crouching = weap_data.kick.auto.standing
 					weap_data.kick.auto.steelsight = weap_data.kick.auto.standing
-				end
-
-				if weap_data.kick.burst then
-					weap_data.kick.burst = weap_data.kick.auto
 				end
 			end
 
@@ -1888,7 +1866,7 @@ Hooks:PostHook(WeaponTweakData, "init", "eclipse_init", function(self, tweak_dat
 	self.erma.reload_speed_multiplier = 1.3
 
 	-- Jackal
-	self.schakal.CLIP_AMMO_MAX = 30
+	self.schakal.CLIP_AMMO_MAX = 25
 	self.schakal.stats.damage = 80
 	self.schakal.stats.spread = 17
 	self.schakal.stats.recoil = 13
