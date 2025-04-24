b08a9d368837915693d8ade048e3d99ef9d16d32
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2025-04-24 12:20:28 +0200

    cft fixes again

--- req/mission_script/chew.lua
+++ req/mission_script/chew.lua
@@ -1,3 +1,5 @@
+local overkill_and_above = Eclipse.utils.diff_threshold()
+
 return {
 	-- Boss spawn
 	[100707] = {
@@ -13,4 +15,14 @@ return {
 			enabled = false,
 		},
 	},
+	-- restore 4 Player C4 Event
+	-- make it appear on ovk above and enable it to all players
+	[100560] = {
+		values = {
+			enabled = not overkill_and_above and false or true,
+			player_1 = true,
+			player_2 = true,
+			player_3 = true,
+		}
+	}
 }
