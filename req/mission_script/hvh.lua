87e5b75b6f7df60c804c0c99fa4067ca9b157afe
hitscanner <1my3mail3@gmail.com>
2025-04-30 20:03:01 +0200

    Update event heist spawns

---
+++ req/mission_script/hvh.lua
@@ -0,0 +1,23 @@
+local preferred = Eclipse.preferred
+local upper_spawn = {
+	values = {
+		interval = 15,
+	},
+}
+local close_spawn = {
+	values = {
+		interval = 30,
+	},
+	groups = preferred.no_shields_bulldozers,
+}
+return {
+	-- Spawn group delays
+	-- This heist is microscopic, thankfully whoever was setting up these spawns bothered to set them to 15s intervals.
+	-- That said, some of them could be slower because they are bit closer to player holdout areas.
+	[100651] = upper_spawn,
+	[101005] = upper_spawn,
+	[101010] = upper_spawn,
+	[100211] = close_spawn,
+	[101007] = close_spawn,
+	[101009] = close_spawn,
+}
--- req/mission_script/nail.lua
+++ req/mission_script/nail.lua
@@ -3,14 +3,18 @@ local is_eclipse = Eclipse.utils.is_eclipse()
 local headless_dozer_black = scripted_enemy.headless_bulldozer_1
 local headless_dozer_white = scripted_enemy.headless_bulldozer_2
 local headless_tanks = is_eclipse and { [headless_dozer_black] = 3, [headless_dozer_white] = 1 } or headless_dozer_black
-
 local headless_tank = {
 	enemy = headless_tanks,
 	values = {
 		participate_to_group_ai = true,
 	},
 }
-
+local ledge_spawn = {
+	values = {
+		interval = 20,
+	},
+	
+}
 return {
 	[100342] = headless_tank,
 	[100343] = headless_tank,
@@ -21,4 +25,14 @@ return {
 	[100348] = headless_tank,
 	[100349] = headless_tank,
 	[100350] = headless_tank,
+	-- Spawn group delays
+	[101601] = ledge_spawn,	
+	[101603] = ledge_spawn,	
+	[101604] = ledge_spawn,	
+	[101605] = ledge_spawn,	
+	[101607] = ledge_spawn,	
+	[101501] = ledge_spawn,	
+	[101799] = ledge_spawn,	
+	[101800] = ledge_spawn,	
+	[101801] = ledge_spawn,	
 }
