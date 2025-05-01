ef6cfffdd3e15d0eb51e8322d00ef160964edf75
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2025-05-01 09:44:59 +0200

    fix typo

--- req/mission_script/bex.lua
+++ req/mission_script/bex.lua
@@ -200,36 +200,38 @@ return {
 	[103003] = disabled,
 	-- add scripted spawns that come out of swat vans
 	[102987] = { 
-	on_executed = {
-		{ id = 400024, delay = 10 }, 
+		on_executed = {
+			{ id = 400024, delay = 10 }, 
+		},
 	},
 	[103002] = { 
-	on_executed = {
-		{ id = 400016, delay = 10 }, 
+		on_executed = {
+			{ id = 400016, delay = 10 },
+		},
 	},
 	-- disable dozers
 	[100018] = {
 		on_executed = {
 			{ id = 400004, delay = 0 },
-		}
+		},
 	},
 	-- enable dozer depending on which swat van arrrived
 	[102989] = {
 		on_executed = {
 			{ id = 400005, delay = 0 },
-		}
+		},
 	},
 	[102988] = {
 		on_executed = {
 			{ id = 400006, delay = 0 },
-		}
+		},
 	},
 	-- spawn the skulldozer that defends your van on Eclipse
 	[101499] = {
 		on_executed = {
 			{ id = 400000, delay = 10 },
 			{ id = 400001, delay = 10 },
-		}
+		},
 	},
 	-- disable guaranteed reenforce in one of the server rooms, the others dont have reenforce, why this one ?
 	[101835] = disabled, -- point area min police force 2
