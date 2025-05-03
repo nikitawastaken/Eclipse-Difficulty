e9773f0dd42e5f325068cb12632cf5e18a3c799d
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2025-05-03 17:43:30 +0200

    A

--- req/mission_script/big.lua
+++ req/mission_script/big.lua
@@ -6,6 +6,7 @@ local is_eclipse = Eclipse.utils.is_eclipse()
 local is_pro_job = Eclipse.utils.is_pro_job()
 local swat_1 = scripted_enemy.swat_1
 local heavy_1 = scripted_enemy.heavy_swat_1
+local elite_sniper = scripted_enemy.elite_sniper
 local light_harasser = swat_1
 local heavy_harasser = is_eclipse and { [heavy_1] = 4, [elite_sniper] = 1 } or heavy_1
 local fail_to_believe_chance = (is_eclipse and 30 or 20) + (is_pro_job and 5 or 0)
