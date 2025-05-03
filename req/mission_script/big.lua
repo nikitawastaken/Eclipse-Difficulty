26643938dc48484fa6a3104c3c210f2db3879c1c
hitscanner <1my3mail3@gmail.com>
2025-05-03 17:42:05 +0200

    NVM

--- req/mission_script/big.lua
+++ req/mission_script/big.lua
@@ -48,7 +48,7 @@ local no_shields_and_dozers = {
 
 local bags_required = {
 	values = {
-		amount = (eclipse and 6 or 4) + (is_pro_job and 2 or 0),
+		amount = (is_eclipse and 6 or 4) + (is_pro_job and 2 or 0),
 	},
 }
 
