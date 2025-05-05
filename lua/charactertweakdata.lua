3a78e4645521159f0dc50db6ec3bab6b21f245e9
hitscanner <1my3mail3@gmail.com>
2025-05-05 18:24:25 +0200

    Give Sniper type enemies focus delay scaling

--- lua/charactertweakdata.lua
+++ lua/charactertweakdata.lua
@@ -305,12 +305,13 @@ function CharacterTweakData:_presets(tweak_data, ...)
 		1 * aim_delay_mul,
 		2 * aim_delay_mul,
 	}
+	presets.weapon.sniper.is_sniper.focus_delay = 0.8 * aim_delay_mul
 
 	presets.weapon.sniper.is_sniper.range = { close = 5000, optimal = 10000, far = 15000 }
 	presets.weapon.sniper.is_sniper.FALLOFF = {
-		{ dmg_mul = 20 * dmg_mul, r = 0, acc = { 0.25, 0.75 }, recoil = { 3, 4 }, mode = { 1, 0, 0, 0 } },
-		{ dmg_mul = 20 * dmg_mul, r = 1000, acc = { 0.5, 1 }, recoil = { 3, 4 }, mode = { 1, 0, 0, 0 } },
-		{ dmg_mul = 20 * dmg_mul, r = 4000, acc = { 0.5, 1 }, recoil = { 3, 4 }, mode = { 1, 0, 0, 0 } },
+		{ dmg_mul = 18 * dmg_mul, r = 0, acc = { 0.25, 0.75 }, recoil = { 3, 4 }, mode = { 1, 0, 0, 0 } },
+		{ dmg_mul = 18 * dmg_mul, r = 1000, acc = { 0.5, 1 }, recoil = { 3, 4 }, mode = { 1, 0, 0, 0 } },
+		{ dmg_mul = 18 * dmg_mul, r = 4000, acc = { 0.5, 1 }, recoil = { 3, 4 }, mode = { 1, 0, 0, 0 } },
 	}
 
 	presets.weapon.fbi_sniper = based_on(presets.weapon.sniper)
@@ -321,6 +322,8 @@ function CharacterTweakData:_presets(tweak_data, ...)
 		0.75 * aim_delay_mul,
 		1.5 * aim_delay_mul,
 	}
+	presets.weapon.elite_sniper.is_sniper.focus_delay = 0.8 * aim_delay_mul
+	
 	presets.weapon.elite_sniper.is_sniper.range = { close = 1500, optimal = 3000, far = 6000 }
 	presets.weapon.elite_sniper.is_sniper.FALLOFF = {
 		{ dmg_mul = 8 * dmg_mul, r = 0, acc = { 0.25, 0.75 }, recoil = { 0.5, 1 }, mode = { 1, 0, 0, 0 } },
