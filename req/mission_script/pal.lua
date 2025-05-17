f83e849666efed8ec73999998f571dce196dd0a9
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2025-05-17 19:27:15 +0200

    Alaskan Deal ambush tweaks

--- req/mission_script/pal.lua
+++ req/mission_script/pal.lua
@@ -130,19 +130,6 @@ return {
 			ignore_disabled = false,
 		},
 	},
-	-- make 2 events happen at the start of the assault on Eclipse
-	[102085] = {
-		values = {
-			amount = swat_vans_and_events,
-			ignore_disabled = false,
-		},
-	},
-	[101685] = {
-		values = {
-			amount = swat_vans_and_events,
-			ignore_disabled = false,
-		},
-	},
 	-- restore Bain's SWAT warnings from PDTH (why they were removed is beyond me)
 	[100714] = {
 		on_executed = {
