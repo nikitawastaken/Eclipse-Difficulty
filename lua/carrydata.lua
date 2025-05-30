b0efb24384204571168c69813dbe1f39b16aa236
hitscanner <1my3mail3@gmail.com>
2025-05-30 11:47:32 +0200

    Tweak bag/rescue conditions

--- lua/carrydata.lua
+++ lua/carrydata.lua
@@ -13,22 +13,13 @@ function CarryData:clbk_pickup_SO_verification(unit)
 	end
 
 	local objective = unit:brain():objective()
-	if not objective or objective.type == "free" or not objective.area then
-		return true
-	end
-
-	if objective.grp_objective and objective.grp_objective.type == "reenforce_area" then
+	if objective and objective.grp_objective and objective.grp_objective.type == "reenforce_area" then
 		return false
 	end
 
 	local logic_data = unit:brain()._logic_data
 	if not logic_data or not logic_data.tactics or logic_data.tactics.rescue then
 		return true
-	end
-
-	local nav_seg = unit:movement():nav_tracker():nav_segment()
-	if objective.area == self._steal_SO_data.pickup_area or self._steal_SO_data.pickup_area.nav_segs[nav_seg] then
-		return objective.area.nav_segs[nav_seg] or managers.groupai:state()._rescue_allowed
 	end
 end
 
