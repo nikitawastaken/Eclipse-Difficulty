4466eb1a92c79d2d97c5fd85ab57a939ff7ea102
hitscanner <1my3mail3@gmail.com>
2025-06-05 19:16:43 +0200

    Revert "Tweak hoxout spawns"
    This reverts commit cecef7b882a3b0aebc8cb7d8c4182b0e10da9b59.

--- req/instance_script/hox_1.lua
+++ req/instance_script/hox_1.lua
@@ -35,7 +35,7 @@ local patches = {
 M["levels/instances/unique/hox_breakout_elevator001/world/world"] = function(result)
 	for _, element in ipairs(result.default.elements) do
 		if patches.elevator_group[element.id] then
-			element.values.interval = 45
+			element.values.interval = 40
 		end
 	end
 end
