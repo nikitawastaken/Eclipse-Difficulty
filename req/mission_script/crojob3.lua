3256e9aea77e5c21894605eddf4ced05bc9fc574
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2025-06-10 19:01:31 +0200

    tweak Bomb Forest's scripted heavy response

--- req/mission_script/crojob3.lua
+++ req/mission_script/crojob3.lua
@@ -1,14 +1,18 @@
 local scripted_enemy = Eclipse.scripted_enemy
 local preferred = Eclipse.preferred
+local is_eclipse = Eclipse.utils.is_eclipse()
 local overkill_and_above = Eclipse.utils.diff_threshold()
+local bulldozer_1 = scripted_enemy.bulldozer_1
 local us_soldier_1 = scripted_enemy.soldier_2
 local us_soldier_2 = scripted_enemy.soldier_3
---local us_soldier_tank = scripted_enemy.soldier_bulldozer
+local us_soldier_tank = scripted_enemy.elite_bulldozer_1
 local us_soldiers = { [us_soldier_1] = 4, [us_soldier_2] = 1 }
 local us_soldier = {
 	enemy = us_soldiers,
 }
---local us_soldier_dozer = overkill_and_above and us_soldier_tank
+local us_soldier_dozer = {
+	enemy = is_eclipse and us_soldier_tank or bulldozer_1,
+}
 local missing_taser_access_fix = {
 	so_access_filter = { "cop", "swat", "tank", "shield", "taser" },
 }
@@ -63,8 +67,7 @@ return {
 	[100675] = missing_taser_access_fix,
 	-- replace heavy response near the end with US Soldiers
 	-- 1st Van (left one)
-	--[100776] = { enemy = us_soldier_dozer },
-	[100555] = us_soldier,
+	[100776] = us_soldier_dozer,
 	[100017] = us_soldier,
 	[100294] = us_soldier,
 	[100295] = us_soldier,
@@ -72,13 +75,9 @@ return {
 	[100297] = us_soldier,
 	[100298] = us_soldier,
 	[100548] = us_soldier,
-	[100767] = us_soldier,
-	[100768] = us_soldier,
+	[100550] = us_soldier,
 	-- 2nd Van (right one)
-	--[100777] = { enemy = us_soldier_dozer },,
-	[100556] = us_soldier,
-	[100549] = us_soldier,
-	[100764] = us_soldier,
+	[100777] = us_soldier_dozer,
 	[100329] = us_soldier,
 	[100330] = us_soldier,
 	[100333] = us_soldier,
@@ -86,10 +85,7 @@ return {
 	[100400] = us_soldier,
 	[100550] = us_soldier,
 	--Far away from vans
-	--[101379] = { enemy = us_soldier_dozer },
-	[101377] = us_soldier,
-	[101375] = us_soldier,
-	[101376] = us_soldier,
+	[101379] = us_soldier_dozer,
 	[101380] = us_soldier,
 	[101381] = us_soldier,
 	[101383] = us_soldier,
@@ -98,10 +94,7 @@ return {
 	[101387] = us_soldier,
 	[101388] = us_soldier,
 	--nearby house
-	--[101363] = { enemy = us_soldier_dozer },
-	[101360] = us_soldier,
-	[101364] = us_soldier,
-	[101365] = us_soldier,
+	[101363] = us_soldier_dozer,
 	[101361] = us_soldier,
 	[101367] = us_soldier,
 	[101368] = us_soldier,
