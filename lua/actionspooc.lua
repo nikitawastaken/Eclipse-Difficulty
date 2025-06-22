90884868b7afdf21f2300de56f1aedbfbdb3f78e
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2025-06-22 12:28:47 +0200

    fix cloaker's lights turn off after beatdown

--- lua/actionspooc.lua
+++ lua/actionspooc.lua
@@ -58,3 +58,38 @@ function ActionSpooc:_chk_target_invalid()
 	local record = managers.groupai:state():criminal_record(self._target_unit:key())
 	return not record or record.status == "disabled" or record.status == "dead"
 end
+
+-- kill the *self._unit:damage():run_sequence_simple("kill_spook_lights")* from on_exit function
+function ActionSpooc:on_exit()
+	if self._unit:character_damage():dead() then
+		self._unit:sound():play(self:get_sound_event("detect_stop"))
+	else
+		self._unit:sound():play(self._unit:base():char_tweak().spawn_sound_event)
+
+		if self._is_local and self._taunt_at_beating_played and not self._unit:sound():speaking(TimerManager:game():time()) then
+			self._unit:sound():say(self._taunt_after_assault, true, true)
+		end
+	end
+
+	if self._root_blend_disabled then
+		self._ext_movement:set_root_blend(true)
+	end
+
+	if self._expired and self._common_data.ext_anim.move then
+		self:_stop_walk()
+	end
+
+	self._ext_movement:drop_held_items()
+
+	if Network:is_server() then
+		local stop_nav_index = math.min(256, self._nav_index - (self._host_stop_pos_inserted or 0))
+
+		self._ext_network:send("action_spooc_stop", mvector3.copy(self._ext_movement:m_pos()), stop_nav_index, self._action_id)
+	else
+		self._ext_movement:set_m_host_stop_pos(self._ext_movement:m_pos())
+	end
+
+	if alive(self._target_unit) and self._target_unit:base().is_local_player then
+		self._target_unit:movement():on_targetted_for_attack(false, self._common_data.unit)
+	end
+end
