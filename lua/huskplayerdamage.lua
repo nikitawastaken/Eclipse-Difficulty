7a71018304e3d993e57f6b4d8455737fa2b2a05f
hitscanner <1my3mail3@gmail.com>
2025-06-26 10:14:09 +0200

    Adjust friendly fire damage

--- lua/huskplayerdamage.lua
+++ lua/huskplayerdamage.lua
@@ -13,15 +13,13 @@ end
 
 function HuskPlayerDamage:damage_fire(attack_data)
 	if managers.mutators:is_mutator_active(MutatorFriendlyFire) or Global.game_settings and Global.game_settings.one_down then
-		attack_data.damage = attack_data.damage * 0.2
-
 		self:_send_damage_to_owner(attack_data)
 	end
 end
 
 function HuskPlayerDamage:_send_damage_to_owner(attack_data)
 	local peer_id = managers.criminals:character_peer_id_by_unit(self._unit)
-	local damage = math.min(20, attack_data.damage ^ 0.9)
+	local damage = math.min(18, attack_data.damage ^ 0.9)
 
 	managers.network:session():send_to_peers("sync_friendly_fire_damage", peer_id, attack_data.attacker_unit, damage, attack_data.variant)
 
