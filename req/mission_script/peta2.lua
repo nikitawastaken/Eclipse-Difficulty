e8415a3ac1be6028dbdca2be1821d43624d9d8de
hitscanner <1my3mail3@gmail.com>
2025-06-28 10:24:07 +0200

    oh peeeta

--- req/mission_script/peta2.lua
+++ req/mission_script/peta2.lua
@@ -1,19 +1,19 @@
 local preferred = Eclipse.preferred
-local farm_spawn1 = {
+local farm_far_spawn = {
 	values = {
-		interval = 15,
+		interval = 10,
 	},
+}
+local farm_close_spawn = {
+	values = {
+		interval = 20,
+	},
+	groups = preferred.no_bulldozers,
 }
 local that_fucking_bush_spawn = {
 	values = {
 		interval = 25,
 	},
-}
-local farm_spawn2 = {
-	values = {
-		interval = 30,
-	},
-	groups = preferred.no_bulldozers,
 }
 return {
 	-- add point of no return
@@ -42,11 +42,11 @@ return {
 	-- Spawn group delays
 	-- Most of the spawns during the farm section are slower now akin to the original version.
 	-- Sadly the weird bush groups (which are split to also spawn enemies under the bridge) are the only spawn groups during the bridge section, so they kind of had to stay fast-ish
-	[100131] = farm_spawn1,
-	[100132] = farm_spawn1,
-	[100133] = farm_spawn1,
-	[100128] = farm_spawn2,
-	[100130] = farm_spawn2,
+	[100131] = farm_far_spawn,
+	[100132] = farm_far_spawn,
+	[100133] = farm_far_spawn,
+	[100128] = farm_close_spawn,
+	[100130] = farm_close_spawn,
 	[101217] = that_fucking_bush_spawn,
 	[102374] = that_fucking_bush_spawn,
 }
