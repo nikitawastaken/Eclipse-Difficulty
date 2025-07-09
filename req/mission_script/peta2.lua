acd034a480f8520d0efe7f7378bd4c593634bdac
rockymoto517 <rockymoto517@gmail.com>
2025-07-09 17:39:09 -0500

    Update old usage of timed groups

--- req/mission_script/peta2.lua
+++ req/mission_script/peta2.lua
@@ -32,7 +32,7 @@ return {
 		values = {
 			callback = function() -- Somebody call the National Guard!
 				if not normal then
-					managers.groupai:state():enabled_timed_group(1)
+					managers.groupai:state():enabled_timed_group("us_scripted_group1")
 				end
 			end,
 		},
