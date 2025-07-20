c3c38c48b722decb4140538bf58e7b4952df68cf
hitscanner43 <hitscanner43@users.noreply.github.com>
2025-07-20 09:32:22 +0000

    Code formatting

--- lua/groupaistatebesiege.lua
+++ lua/groupaistatebesiege.lua
@@ -311,7 +311,7 @@ function GroupAIStateBesiege:_upd_reenforce_tasks()
 
 			-- Adjust next reinforce dispatch time based on the amount of tasks still needed
 			local min_reenforce_interval = self:_get_difficulty_dependent_value(tweak_data.group_ai.min_reenforce_interval)
-			
+
 			if spawned then
 				--self._task_data.reenforce.next_dispatch_t = self._t + self:_get_difficulty_dependent_value(self._tweak_data.reenforce.interval) / #undershot_tasks
 				self._task_data.reenforce.next_dispatch_t = self._t + math.max(min_reenforce_interval, self:_get_difficulty_dependent_value(self._tweak_data.reenforce.interval) / #undershot_tasks)
