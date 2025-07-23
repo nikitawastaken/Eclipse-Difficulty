3a68012037ad5356ca9db512f71af85353e0724d
hitscanner <1my3mail3@gmail.com>
2025-07-23 19:23:55 +0200

    Update bex

--- req/mission_script/bex.lua
+++ req/mission_script/bex.lua
@@ -116,7 +116,7 @@ return {
 		},
 	},
 	-- Reinforce second floor above tellers
-	[101822] = { -- Wait for drill parts
+	[100123] = { -- Assault done 
 		reinforce = {
 			{
 				name = "teller_balcony1",
