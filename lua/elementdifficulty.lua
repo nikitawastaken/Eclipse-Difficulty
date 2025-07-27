67362bd78bec84979895c218eda69b1a3e58d69e
hitscanner <1my3mail3@gmail.com>
2025-07-27 09:44:28 +0200

    Remove debug

--- lua/elementdifficulty.lua
+++ lua/elementdifficulty.lua
@@ -1,6 +1,4 @@
 --ElementDifficulty
 Hooks:OverrideFunction(ElementDifficulty, "on_executed", function(...)
-	Eclipse:log_chat("Blocked element from setting diff.")
-
-	ElementDifficulty.super.on_executed(...)
+    ElementDifficulty.super.on_executed(...)
 end)
