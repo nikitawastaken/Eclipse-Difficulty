c1fd7b002ba515af46ba7a758de232dd9596db86
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2025-08-15 18:14:56 +0200

    Update election_day_2.lua

--- req/mission_script_add/election_day_2.lua
+++ req/mission_script_add/election_day_2.lua
@@ -55,8 +55,8 @@ M.elements = {
 	Eclipse.mission_elements.gen_dummy(400010, "swat_van_spawn_7", Vector3(-2292.224, 2016.435, -19.999), Rotation(74, 0, 0), optsBesiegeDummy),
 	Eclipse.mission_elements.gen_dummy(400011, "swat_van_spawn_8", Vector3(-2310.692, 1952.031, -19.999), Rotation(74, 0, 0), optsBesiegeDummy),
 	Eclipse.mission_elements.gen_missionscript(400012, "spawn_swats_2", optsspawnvanSWATs_2),
-	Eclipse.mission_elements.gen_object_editor(400013, "open_swat_doors_2", Vector3(0, 0, 0), Rotation(0, 0, -0), optsOpenSwatVanDoors_1),
-	Eclipse.mission_elements.gen_spawngroup(400014, "swat_group_2", { 400001, 400002, 400003, 400004 }, 0, opts_swat_group),
+	Eclipse.mission_elements.gen_object_editor(400013, "open_swat_doors_2", Vector3(0, 0, 0), Rotation(0, 0, -0), optsOpenSwatVanDoors_2),
+	Eclipse.mission_elements.gen_spawngroup(400014, "swat_group_2", { 400008, 400009, 400010, 400011 }, 0, opts_swat_group),
 }
 
 return M
