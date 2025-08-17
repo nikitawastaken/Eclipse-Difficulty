a82f340ade2c03795f8bbe792498d1416b3b46e4
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2025-08-17 16:48:19 +0200

    more dangerous choppers in Bomb Forest

--- req/mission_script/crojob3.lua
+++ req/mission_script/crojob3.lua
@@ -1,13 +1,28 @@
 local scripted_enemy = Eclipse.scripted_enemy
 local preferred = Eclipse.preferred
+local normal, hard, eclipse = Eclipse.utils.diff_groups()
 local is_eclipse = Eclipse.utils.is_eclipse()
-local overkill_and_above = Eclipse.utils.diff_threshold()
+local is_pro_job = Eclipse.utils.is_pro_job()
+local hard_and_above, overkill_and_above = Eclipse.utils.diff_threshold()
 local bulldozer_1 = scripted_enemy.bulldozer_1
 local us_soldier_1 = scripted_enemy.soldier_2
 local us_soldier_2 = scripted_enemy.soldier_3
 local us_soldier_3 = scripted_enemy.soldier_4
 local us_soldier_tank = scripted_enemy.elite_bulldozer_1
 local us_soldiers = { [us_soldier_1] = 4, [us_soldier_2] = 2, [us_soldier_3] = 1 }
+local green_bulldozer = scripted_enemy.bulldozer_1
+local black_bulldozer = scripted_enemy.bulldozer_2
+local elite_ben_bulldozer = scripted_enemy.elite_bulldozer_1
+local elite_skull_bulldozer = scripted_enemy.elite_bulldozer_2
+local cloaker = scripted_enemy.cloaker
+local medic = scripted_enemy.medic_1
+local taser = scripted_enemy.taser_1
+local specials_list_eclipse = { [taser] = 2, [medic] = 2, [cloaker] = 2, [elite_ben_bulldozer] = 1, [elite_skull_bulldozer] = 1 }
+local specials_list_hard_ovk = { [taser] = 4, [medic] = 3, [cloaker] = 2, [green_bulldozer] = 1, [black_bulldozer] = 1 }
+local specials_list_easy_normal = { [taser] = 3, [cloaker] = 1 }
+local specials = {
+	enemy = normal and specials_list_easy_normal or hard and specials_list_hard_ovk or specials_list_eclipse,
+}
 local us_soldier = {
 	enemy = us_soldiers,
 }
@@ -17,6 +32,13 @@ local us_soldier_dozer = {
 local missing_taser_access_fix = {
 	so_access_filter = { "cop", "swat", "tank", "shield", "taser" },
 }
+local filter_easy_above = {
+	values = Eclipse.utils.set_diff_groups("easy_above"),
+}
+local filter_disable = {
+	values = Eclipse.utils.set_diff_groups("disable"),
+}
+local chopper_amount = (is_eclipse and 2 or 1) + (is_pro_job and 1 or 0)
 local flank_spawn = {
 	values = {
 		interval = 15,
@@ -49,6 +71,45 @@ return {
 	},
 	-- fix one of the ai_hunt SOs not having taser access
 	[100675] = missing_taser_access_fix,
+	-- replace scripted spawns that drop from choppa with US Soldiers and specials
+	-- filters (to keep it clean)
+	[104152] = filter_easy_above,
+	[104153] = filter_easy_above,
+	[104156] = filter_easy_above,
+	[104147] = filter_disable,
+	[104148] = filter_disable,
+	[104154] = filter_disable,
+	[104155] = filter_disable,
+	[104157] = filter_disable,
+	[104158] = filter_disable,
+	-- 1 and 3 dummies are soldiers, the rest are specials (similiar to Train Heist)
+	-- 1st chopper
+	[101177] = us_soldier,
+	[101174] = specials,
+	[101176] = us_soldier,
+	[101175] = specials,
+	-- 2nd chopper
+	[104176] = us_soldier,
+	[104177] = specials,
+	[104174] = us_soldier,
+	[104175] = specials,
+	-- 3rd chopper
+	[100484] = us_soldier,
+	[100485] = specials,
+	[100482] = us_soldier,
+	[100479] = specials,
+	-- don't disable the choppers when you pick up the bomb part
+	[104312] = {
+		on_executed = {
+			{ id = 104312, remove = true },
+		},
+	},
+	-- more choppers on eclipse and pro jobs
+	[101172] = {
+		values = {
+			amount = chopper_amount,
+		},
+	},
 	-- replace heavy response near the end with US Soldiers
 	-- 1st Van (left one)
 	[100776] = us_soldier_dozer,
