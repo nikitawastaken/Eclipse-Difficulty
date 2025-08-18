122545498d2c13c69e1ce1f29ea2648a28c54856
hitscanner <1my3mail3@gmail.com>
2025-08-18 20:27:55 +0200

    a

--- lua/drill.lua
+++ lua/drill.lua
@@ -1,25 +1,18 @@
 local level_id = Eclipse.utils.level_id()
 
+Drill.drill_reenforce_blacklist = Eclipse.drill_reenforce_blacklist
 Drill.forbid_sabotage_SO_by_unit = {
 	[("units/payday2/equipment/gen_interactable_lance_huge/gen_interactable_lance_huge"):key()] = true, -- The Beast
 	[("units/payday2/equipment/gen_interactable_lance_large/gen_interactable_lance_large"):key()] = level_id == "red2" or nil, -- Ordinary thermal lance
-}
-Drill.no_automatic_drill_reenforce = {
-	["arm_fac"] = true,
-	["arm_par"] = true,
-	["arm_hcm"] = true,
-	["arm_und"] = true,
-	["arm_cro"] = true,
-	["arm_for"] = true,
-	["glace"] = true,
 }
 
 Hooks:PostHook(Drill, "init", "eclipse_init", function(self, unit)
 	self._sabotage_SO_forbidden = self.forbid_sabotage_SO_by_unit[unit:name():key()] or nil
 end)
 
+-- Mark drills for reinforce groups
 Hooks:PostHook(Drill, "start", "eclipse_start", function(self)
-	if not self.no_automatic_drill_reenforce[level_id] then
+	if not self.drill_reenforce_blacklist[level_id] then
 		managers.groupai:state():set_area_min_police_force(self._unit:key(), 2, self._unit:position())
 	end
 end)
