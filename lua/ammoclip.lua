6dd9043987b01603fac6a0b1f9eaed0b49250f27
nikitawastaken <59087849+nikitawastaken@users.noreply.github.com>
2025-08-20 01:32:16 +0300

    Revert "attempt to fix gambler heal share not working"
    This reverts commit d5ce9b6ab73800cda90d03b4d99b2261fa542aa3.

--- lua/ammoclip.lua
+++ lua/ammoclip.lua
@@ -99,46 +99,40 @@ function AmmoClip:_pickup(unit)
 	return false
 end
 
-local origfunc = AmmoClip.sync_net_event
-function AmmoClip:sync_net_event(event, peer, ...)
+function AmmoClip:sync_net_event(event, peer)
+	local player = managers.player:local_player()
+
+	if not alive(player) or not player:character_damage() or player:character_damage():is_downed() or player:character_damage():dead() then
+		return
+	end
+	Eclipse:log_chat("sync net event received, event: " .. event)
+
 	if event == AmmoClip.EVENT_IDS.bonnie_share_ammo then
-		local player = managers.player:local_player()
-
-		if not alive(player) or not player:character_damage() or player:character_damage():is_downed() or player:character_damage():dead() then
-			return
-		end
-
+		Eclipse:log_chat("first ammo restore check passed")
 		local inventory = player:inventory()
 
 		if inventory then
-			local picked_up, add_amount = false, nil
+			local picked_up = false
 
 			for id, weapon in pairs(inventory:available_selections()) do
-				picked_up, add_amount = weapon.unit:base():add_ammo(tweak_data.upgrades.loose_ammo_give_team_ratio or 0.25) or picked_up
-				--------------------------------------------------
-				if picked_up and (not add_amount or add_amount < 1) then
-					local wub = weapon.unit:base()
-					if wub and wub._ammo_pickup and wub._ammo_pickup[2] < 2 then
-						local prob = ((wub._ammo_pickup[1] + wub._ammo_pickup[2]) / 2) * tweak_data.upgrades.loose_ammo_give_team_ratio
-						if prob > math.random() then
-							picked_up, add_amount = wub:add_ammo(nil, 1) or picked_up
-						end
-					end
-				end
-				--------------------------------------------------
+				picked_up = weapon.unit:base():add_ammo(tweak_data.upgrades.loose_ammo_give_team_ratio or 0.25) or picked_up
 			end
 
 			if picked_up then
-				Eclipse:log_chat("client restored ammo")
 				player:sound():play(self._pickup_event or "pickup_ammo", nil, true)
 
 				for id, weapon in pairs(inventory:available_selections()) do
 					managers.hud:set_ammo_amount(id, weapon.unit:base():ammo_info())
 				end
+				Eclipse:log_chat("client restored ammo")
 			end
 		end
+	elseif event == AmmoClip.EVENT_IDS.register_grenade then
+		if peer and not self._grenade_registered then
+			managers.player:register_grenade(peer:id())
 
-		return
+			self._grenade_registered = true
+		end
 	elseif event < AmmoClip.EVENT_IDS.bonnie_share_ammo then
 		Eclipse:log_chat("first health restore check passed")
 		local damage_ext = player:character_damage()
@@ -152,8 +146,5 @@ function AmmoClip:sync_net_event(event, peer, ...)
 				Eclipse:log_chat("client restored health")
 			end
 		end
-
-		return
 	end
-	return origfunc(self, event, peer, ...)
 end
