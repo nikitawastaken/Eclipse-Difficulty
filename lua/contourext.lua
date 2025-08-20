75bafc30a06035087b3339af86d216cfa014d53f
hitscanner43 <hitscanner43@users.noreply.github.com>
2025-08-20 20:33:14 +0000

    Code formatting

--- lua/contourext.lua
+++ lua/contourext.lua
@@ -24,8 +24,8 @@ end
 local add_original = ContourExt.add
 function ContourExt:add(...)
 	local result = add_original(self, ...)
-	
-	call_on_next_update(function() 
+
+	call_on_next_update(function()
 		return result
 	end)
 end
