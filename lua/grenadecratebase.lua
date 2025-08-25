d854c3e36b4a827a80e4ffb21b13c10561d05064
nikitawastaken <nikitawastaken@users.noreply.github.com>
2025-08-25 17:25:26 +0000

    Code formatting

--- lua/grenadecratebase.lua
+++ lua/grenadecratebase.lua
@@ -16,7 +16,7 @@ end
 
 function GrenadeCrateBase:set_server_information(peer_id)
 	self._server_information = {
-		owner_peer_id = peer_id
+		owner_peer_id = peer_id,
 	}
 
 	managers.network:session():peer(peer_id):set_used_deployable(true)
