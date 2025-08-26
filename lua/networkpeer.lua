22caf50b9443530f95493d715e54f81c6d1ce3c6
rockymoto517 <rockymoto517@gmail.com>
2025-08-25 19:40:02 -0500

    Make anticheat logging use our own logfile

--- lua/networkpeer.lua
+++ lua/networkpeer.lua
@@ -56,6 +56,6 @@ function NetworkPeer:verify_grenade(value)
 end
 
 Hooks:PostHook(NetworkPeer, "mark_cheater", "eclipse_mark_cheater_debug", function()
-	Eclipse:log_chat("[DEBUG] You were marked as cheater!\nSend the stacktrace from your log file to a developer and see if you can reproduce this so we can patch this out.")
-	log("[ECLIPSE ANTI-CHEAT DEBUG]:\n", debug.traceback(), "\nEND STACKTRACE\n\n")
+	Eclipse:log_chat("[DEBUG] You were marked as cheater!\nSend the stacktrace from your log file in the eclipse folder to a developer and see if you can reproduce this so we can patch this out.")
+	Eclipse.log(string.format("[ECLIPSE ANTI-CHEAT DEBUG]:\n%s\nEND STACKTRACE\n\n", debug.traceback()))
 end)
