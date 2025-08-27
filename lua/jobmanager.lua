1d76c1f96b264125320cc0b14fa6af8a734d7584
hitscanner43 <hitscanner43@users.noreply.github.com>
2025-08-27 16:02:55 +0000

    Code formatting

--- lua/jobmanager.lua
+++ lua/jobmanager.lua
@@ -6,7 +6,7 @@ function JobManager:current_spawn_limit(special_type)
 	local level_data = self:current_level_data()
 	local is_skirmish = level_data and level_data.group_ai_state == "skirmish"
 	local special_limit_balance_mul = managers.groupai:state():_get_balancing_multiplier(tweak_data.group_ai.special_unit_spawn_limits_balance_mul)
-	
+
 	if is_skirmish then
 		local limits_table = tweak_data.skirmish.special_unit_spawn_limits
 		local wave_number = managers.groupai:state():get_assault_number()
