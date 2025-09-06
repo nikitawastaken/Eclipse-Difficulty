f799a681ba8b29fa4c6706750995289c30ee5f82
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2025-09-06 17:41:55 +0200

    edit hox revenge's chopper

---
+++ req/instance_script/hox_3.lua
@@ -0,0 +1,51 @@
+---@module Hoxton Revenge
+local M = {}
+local scripted_enemy = Eclipse.scripted_enemy
+local so_access = Eclipse.access_filter
+local normal, hard, eclipse = Eclipse.utils.diff_groups()
+local fbi_ready_team = scripted_enemy.ready_team_1
+local green_bulldozer = scripted_enemy.bulldozer_1
+local black_bulldozer = scripted_enemy.bulldozer_2
+local elite_ben_bulldozer = scripted_enemy.elite_bulldozer_1
+local elite_skull_bulldozer = scripted_enemy.elite_bulldozer_2
+local cloaker = scripted_enemy.cloaker
+local medic = scripted_enemy.medic_1
+local taser = scripted_enemy.taser_1
+local specials_list_eclipse = { [taser] = 2, [medic] = 2, [cloaker] = 2, [elite_ben_bulldozer] = 1, [elite_skull_bulldozer] = 1 }
+local specials_list_hard_ovk = { [taser] = 4, [medic] = 3, [cloaker] = 2, [green_bulldozer] = 1, [black_bulldozer] = 1 }
+local specials_list_easy_normal = { [taser] = 3, [cloaker] = 1 }
+local specials = normal and specials_list_easy_normal or hard and specials_list_hard_ovk or specials_list_eclipse
+local law = so_access.law
+local filter_disable = Eclipse.utils.set_diff_groups("disable")
+local filter_normal_above = Eclipse.utils.set_diff_groups("easy_above")
+local patches = {
+	heli = {
+		fbi_ready_teams = table.set(100003, 100000),
+		specials = table.set(100002, 100004),
+		hunt_so = table.set(100011),
+		filters_disable = table.set(100042, 100075, 100033),
+		filters_normal_above = table.set(100041),
+	},
+}
+
+return {
+	["levels/instances/unique/hox_estate_heli_spawn/world/world"] = function(result)
+		local heli_fbi = patches.heli
+
+		for _, element in pairs(result.default.elements) do
+			local id = element.id
+
+			if heli_fbi.fbi_ready_teams[id] then
+				element.values.enemy = fbi_ready_team
+			elseif heli_fbi.specials[id] then
+				element.values.enemy_table = specials
+			elseif heli_fbi.hunt_so[id] then
+				element.values.SO_access = law
+			elseif heli_fbi.filters_normal_above[id] then
+				table.map_append(element.values, filter_normal_above)
+			elseif heli_fbi.filters_disable[id] then
+				table.map_append(element.values, filter_disable)
+			end
+		end
+	end,
+}
