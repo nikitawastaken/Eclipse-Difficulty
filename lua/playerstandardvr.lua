ebc3953b5c5dc4c6fdc7e3209030966df5933cb3
Groxxy <54233743+InsaneGrox@users.noreply.github.com>
2025-09-08 17:08:59 -0400

    Fix VR specific crash when firing weapons
    linear_lerp no longer exists xdd

--- lua/playerstandardvr.lua
+++ lua/playerstandardvr.lua
@@ -167,7 +167,7 @@ function PlayerStandardVR:_check_fire_per_weapon(t, pressed, held, released, wea
 			local recoil_multiplier = (weap_base:recoil() + weap_base:recoil_addend()) * weap_base:recoil_multiplier()
 			local weap_tweak_data = tweak_data.weapon[weap_base:get_name_id()]
 			local shake_tweak_data = weap_tweak_data.shake[fire_mode] or weap_tweak_data.shake
-			local recoil_shake = linear_lerp(recoil_multiplier, 0.5, 3, 0.8, 1.2)
+			local recoil_shake = math.map_range(recoil_multiplier, 0.5, 3, 0.8, 1.2)
 			local shake_multiplier = shake_tweak_data["fire_multiplier"] * recoil_shake
 
 			if self._state_data.in_steelsight then
