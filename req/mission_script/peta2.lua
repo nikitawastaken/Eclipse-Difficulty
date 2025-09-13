c18b75143c98b831e3e5aac6681d98ae6eb281e4
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2025-09-13 20:39:29 +0200

    fix peta2's softlock

--- req/mission_script/peta2.lua
+++ req/mission_script/peta2.lua
@@ -7,7 +7,7 @@ local goats_required = {
 }
 local random_goats = {
 	values = {
-		amount = (normal and 2 or hard and 5 or 7) + (is_pro_job and 2 or 0),
+		amount = (normal and 5 or hard and 8 or 10) + (is_pro_job and 2 or 0),
 	},
 }
 local disabled = {
