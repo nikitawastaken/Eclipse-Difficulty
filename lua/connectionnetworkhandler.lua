3aa8856b2a2f259f9f6b275b7131e95a923ec455
rockymoto517 <rockymoto517@gmail.com>
2025-09-13 16:31:08 -0500

    Fix faulty xml tweak

--- lua/connectionnetworkhandler.lua
+++ lua/connectionnetworkhandler.lua
@@ -1,17 +1,6 @@
-function ConnectionNetworkHandler:finish_trade(is_recon_over, resource_trade)
-	if self._verify_gamestate(self._gamestate_filter.waiting_for_respawn) and not resource_trade then
+function ConnectionNetworkHandler:finish_trade()
+	if self._verify_gamestate(self._gamestate_filter.waiting_for_respawn) then
 		game_state_machine:current_state():finish_trade()
-	else
-		-- Do not do a resource trade if someone is in civilian mode or is in custody
-		local ingame_has_inv = table.filter(self._gamestate_filter.any_ingame_playing, function(k, _)
-			if table.contains({ "ingame_waiting_for_respawn", "ingame_waiting_for_spawn_allowed", "ingame_civilian" }, k) then
-				return false
-			end
-			return true
-		end)
-		if self._verify_gamestate(ingame_has_inv) and resource_trade then
-			game_state_machine:current_state():finish_resource_trade(is_recon_over)
-		end
 	end
 end
 
