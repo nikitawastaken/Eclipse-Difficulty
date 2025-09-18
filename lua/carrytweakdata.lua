16cb2d549c1518315cce22b88265296d73d01a1f
hitscanner <1my3mail3@gmail.com>
2025-09-18 18:33:36 +0200

    Increase bag move speeds

--- lua/carrytweakdata.lua
+++ lua/carrytweakdata.lua
@@ -1,7 +1,8 @@
 Hooks:PostHook(CarryTweakData, "init", "eclipse_init", function(self)
 	-- Heavy bags no longer reduce jump height
 	for _, bag_type in pairs(self.types) do
-		bag_type.jump_modifier = 1
+		bag_type.move_speed_modifier = math.min(bag_type.move_speed_modifier + 0.15, 1)
+		bag_type.jump_modifier = math.min(bag_type.jump_modifier + 0.25, 1)		
 	end
 
 	-- Change the weight of specific loot types
