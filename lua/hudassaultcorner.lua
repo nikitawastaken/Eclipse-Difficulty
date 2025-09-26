ef12969c5cab297a2a0cce339d9f6c1511cd1770
hitscanner <1my3mail3@gmail.com>
2025-09-26 21:01:01 +0200

    Potentially fix disappearing assault banner

--- lua/hudassaultcorner.lua
+++ lua/hudassaultcorner.lua
@@ -1,4 +1,6 @@
 local level_id = Eclipse.utils.level_id()
+local is_eclipse = Eclipse.utils.is_eclipse()
+local faction = Eclipse.utils.faction()
 local has_ponr_text = Eclipse.settings.ponr_assault_text
 local has_faction_text = Eclipse.settings.faction_assault_text
 
@@ -10,43 +12,10 @@ function HUDAssaultCorner:set_ponr_state()
 	self._ponr_state = true
 end
 
-if has_ponr_text then
-	function HUDAssaultCorner:sync_start_assault(assault_number)
-		local is_ponr = managers.groupai:state_name() == "ponr" or self._ponr_state
-
-		if self._point_of_no_return or self._casing then
-			return
-		end
-
-		local color = self._assault_color
-		local diff_i = tweak_data:difficulty_to_index(Global.game_settings and Global.game_settings.difficulty or "normal")
-
-		if is_ponr then
-			if diff_i == 6 then
-				color = Color.red
-			else
-				color = self._vip_assault_color
-			end
-		end
-
-		if self._assault_mode == "phalanx" then
-			color = self._vip_assault_color
-		end
-
-		self:_update_assault_hud_color(color)
-		self:set_assault_wave_number(assault_number)
-
-		self._start_assault_after_hostage_offset = true
-
-		self:_set_hostage_offseted(true)
-	end
-end
-
 function HUDAssaultCorner:_get_assault_strings()
 	local is_ponr = managers.groupai:state_name() == "ponr" or self._ponr_state
 
-	local level_faction = tweak_data.levels[level_id] and tweak_data.levels[level_id].ai_group_type
-	local primary_assault_text = has_faction_text and "hud_assault_" .. level_faction .. "_assault" or "hud_assault_assault"
+	local primary_assault_text = has_faction_text and "hud_assault_" .. faction .. "_assault" or "hud_assault_assault"
 	local secondary_assault_text = has_ponr_text and is_ponr and "hud_assault_ponr" or primary_assault_text
 
 	if self._assault_mode == "normal" then
@@ -69,10 +38,12 @@ function HUDAssaultCorner:_get_assault_strings()
 				"hud_assault_end_line",
 				primary_assault_text,
 				"hud_assault_end_line",
+				primary_assault_text,
+				"hud_assault_end_line"
 			}
 		end
 	end
-	--[[
+
 	if self._assault_mode == "phalanx" then
 		if managers.job:current_difficulty_stars() > 0 then
 			local ids_risk = Idstring("risk")
@@ -98,5 +69,35 @@ function HUDAssaultCorner:_get_assault_strings()
 			}
 		end
 	end
-]]
+end
+
+if has_ponr_text then
+	function HUDAssaultCorner:sync_start_assault(assault_number)
+		local is_ponr = managers.groupai:state_name() == "ponr" or self._ponr_state
+
+		if self._point_of_no_return or self._casing then
+			return
+		end
+
+		local color = self._assault_color
+
+		if is_ponr then
+			if is_eclipse then
+				color = Color.red
+			else
+				color = self._vip_assault_color
+			end
+		end
+
+		if self._assault_mode == "phalanx" then
+			color = self._vip_assault_color
+		end
+
+		self:_update_assault_hud_color(color)
+		self:set_assault_wave_number(assault_number)
+
+		self._start_assault_after_hostage_offset = true
+
+		self:_set_hostage_offseted(true)
+	end
 end
