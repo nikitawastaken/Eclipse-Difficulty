7889c2d583ad57d6be207c75774a11d95e48ddba
hitscanner <1my3mail3@gmail.com>
2025-09-28 17:58:42 +0200

    Revert "comment you out for now"
    This reverts commit 1bad79316c9420821115e1bae01568f216e57708.

--- lua/connectionnetworkhandler.lua
+++ lua/connectionnetworkhandler.lua
@@ -1,4 +1,3 @@
---[[
 function ConnectionNetworkHandler:sync_assault_ponr(sender)
 	local peer = self._verify_sender(sender)
 	if not peer then
@@ -9,7 +8,6 @@ function ConnectionNetworkHandler:sync_assault_ponr(sender)
 
 	managers.hud._hud_assault_corner:set_ponr_state()
 end
-]]
 
 function ConnectionNetworkHandler:sync_trade_restore_resources(sender)
 	if not self._verify_sender(sender) or not self._verify_gamestate(self._gamestate_filter.any_ingame_playing) or not alive(managers.player:player_unit()) then
