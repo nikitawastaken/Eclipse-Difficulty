6743a00e47d29a104391270b8a004bc2175abbe1
rockymoto517 <rockymoto517@gmail.com>
2025-10-09 18:39:05 -0500

    move playercarryvr to playercarry cause they load in the wrong order
    Co-Authored-By: Farion <latinfaun900@gmail.com>

--- lua/playercarry.lua
+++ lua/playercarry.lua
@@ -215,3 +215,27 @@ function PlayerCarry:_get_walk_headbob(...)
 	multiplier = math.clamp(multiplier * managers.player:upgrade_value("player", "mrwi_carry_speed_multiplier", 1), 0, 1)
 	return PlayerCarry.super._get_walk_headbob(self, ...) * multiplier
 end
+
+if _G.IS_VR then
+	function PlayerCarryVR:_can_run()
+		local can_run = true
+		for _, name in pairs(self._tweak_data_name) do
+			if tweak_data.carry.types[name] and not tweak_data.carry.types[name].can_run then
+				can_run = false
+				break
+			end
+		end
+
+		return can_run or managers.player:has_category_upgrade("carry", "movement_penalty_nullifier")
+	end
+
+	function PlayerCarryVR:_check_use_item(t, input)
+		if not input.btn_throw_bag_press then
+			return PlayerStandard._check_use_item(self, t, input)
+		end
+
+		managers.player:drop_carry()
+		managers.player:player_unit():movement():current_state():set_throwing_projectile(self._unit:hand():get_active_hand_id("bag"))
+		return true
+	end
+end
