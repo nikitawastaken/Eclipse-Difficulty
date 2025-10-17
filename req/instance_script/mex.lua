5675a8a318d365f4519906c666a2967fbb7b61ec
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2025-10-17 10:15:55 +0200

    delay interruptions of mex's pump/tanker

--- req/instance_script/mex.lua
+++ req/instance_script/mex.lua
@@ -1,6 +1,7 @@
 ---@module Border Crossing
 local M = {}
 local patches = {
+	tanker_and_pump_interruption_delay = table.set(100017),
 	vault = {
 		prevent_disabling_the_vault_timer = table.set(100008),
 	},
@@ -14,6 +15,26 @@ M["levels/instances/unique/mex/mex_vault/world/world"] = function(result)
 
 		if mex_vault.prevent_disabling_the_vault_timer[id] then
 			element.values.enabled = false
+		end
+	end
+end
+M["levels/instances/unique/mex/mex_tanker/world/world"] = function(result)
+	for _, element in ipairs(result.default.elements) do
+		if patches.tanker_and_pump_interruption_delay[element.id] then
+			element.values.on_executed = {
+				{ id = 100018, delay = 20, delay_rand = 10 }, -- delay the interrupt of the tanker to 20-30 seconds (from 5 seconds in vanilla)
+				{ id = 100038, delay = 1 }, -- defend icon waypoint
+			}
+		end
+	end
+end
+M["levels/instances/unique/mex/mex_pump/world/world"] = function(result)
+	for _, element in ipairs(result.default.elements) do
+		if patches.tanker_and_pump_interruption_delay[element.id] then
+			element.values.on_executed = {
+				{ id = 100018, delay = 20, delay_rand = 10 }, -- delay the interrupt of the pump to 20-30 seconds (from 5 seconds in vanilla
+				{ id = 100038, delay = 1 }, -- defend icon waypoint
+			}
 		end
 	end
 end
