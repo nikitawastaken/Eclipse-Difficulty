e2e0a55091de9c8e050458fa29afd8d0f2fa8889
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2025-10-19 20:49:49 +0200

    delay interruptions of wwh's pump/tanker

---
+++ req/instance_script/wwh.lua
@@ -0,0 +1,26 @@
+---@module Alaskan Deal
+local M = {}
+local patches = {
+	tanker_and_pump_interruption_delay = table.set(100017),
+}
+
+M["levels/instances/unique/wwh/wwh_tanker/world/world"] = function(result)
+	for _, element in ipairs(result.default.elements) do
+		if patches.tanker_and_pump_interruption_delay[element.id] then
+			element.values.on_executed = {
+				{ id = 100018, delay = 10, delay_rand = 20 }, -- delay the interrupt of the tanker to 10-30 seconds (from 0 seconds in vanilla)
+			}
+		end
+	end
+end
+M["levels/instances/unique/wwh/wwh_pump/world/world"] = function(result)
+	for _, element in ipairs(result.default.elements) do
+		if patches.tanker_and_pump_interruption_delay[element.id] then
+			element.values.on_executed = {
+				{ id = 100018, delay = 10, delay_rand = 20 }, -- delay the interrupt of the pump to 10-30 seconds (from 0 seconds in vanilla)
+			}
+		end
+	end
+end
+
+return M
