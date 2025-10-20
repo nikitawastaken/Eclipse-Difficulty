91b98bae20ccfc9723870a1c196b17e7e11a00d5
hitscanner <121458955+hitscanner43@users.noreply.github.com>
2025-10-20 18:47:54 +0200

    Update player flashbangs
    - Slightly reduced flash range
    - Slightly reduce flash duration
    - Reverted the sound effect change

--- lua/concussiongrenade.lua
+++ lua/concussiongrenade.lua
@@ -1,19 +1,14 @@
-Hooks:PostHook(ConcussionGrenade, "_setup_from_tweak_data", "Eclipse_concussion", function(self)
-	self._custom_params = {
-		camera_shake_max_mul = 0.75, -- reduce screenshake
-		effect = self._effect_name,
-		sound_event = "flashbang_explosion", -- flashbang sfx
-		feedback_range = self._range * 2,
-	}
+Hooks:PostHook(ConcussionGrenade, "_setup_from_tweak_data", "eclipse_setup_from_tweak_data", function(self)
+	self._custom_params.camera_shake_max_mul = 0.75, -- reduce screenshake
 end)
 
 function ConcussionGrenade:_flash_player()
 	local detonate_pos = self._unit:position() + math.UP * 100
-	local range = 650
+	local range = self._PLAYER_FLASH_RANGE
 	local affected, line_of_sight, travel_dis, linear_dis = QuickFlashGrenade._chk_dazzle_local_player(self, detonate_pos, range)
 
 	if affected then
-		managers.environment_controller:set_flashbang(detonate_pos, line_of_sight, travel_dis, linear_dis, tweak_data.character.flashbang_multiplier, nil, true)
+		managers.environment_controller:set_flashbang(detonate_pos, line_of_sight, travel_dis, linear_dis, tweak_data.character.flashbang_multiplier * 0.75, nil, true)
 
 		local sound_eff_mul = math.clamp(1 - (travel_dis or linear_dis) / range, 0.3, 1)
 
