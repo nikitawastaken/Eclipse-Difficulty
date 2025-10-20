6ec7a38480e3594de0f3b6d257ed90d6da564e29
hitscanner <121458955+hitscanner43@users.noreply.github.com>
2025-10-20 18:54:28 +0200

    Remove flash duration diff scaling from player flashes

--- lua/concussiongrenade.lua
+++ lua/concussiongrenade.lua
@@ -8,7 +8,7 @@ function ConcussionGrenade:_flash_player()
 	local affected, line_of_sight, travel_dis, linear_dis = QuickFlashGrenade._chk_dazzle_local_player(self, detonate_pos, range)
 
 	if affected then
-		managers.environment_controller:set_flashbang(detonate_pos, line_of_sight, travel_dis, linear_dis, tweak_data.character.flashbang_multiplier * 0.75, nil, true)
+		managers.environment_controller:set_flashbang(detonate_pos, line_of_sight, travel_dis, linear_dis, 0.8, nil, true)
 
 		local sound_eff_mul = math.clamp(1 - (travel_dis or linear_dis) / range, 0.3, 1)
 
