b0dabbe0cc9150abaa35f70c938732a122d841c6
hitscanner <121458955+hitscanner43@users.noreply.github.com>
2025-10-22 19:58:40 +0200

    Sentry guns no longer proc grinder

--- lua/playermanager.lua
+++ lua/playermanager.lua
@@ -1874,6 +1874,62 @@ function PlayerManager:spawn_extra_ammo(killed_unit, requesting_peer, has_extra_
 	end
 end
 
+function PlayerManager:_check_damage_to_hot(t, unit, damage_info)
+	local player_unit = self:player_unit()
+
+	if not self:has_category_upgrade("player", "damage_to_hot") then
+		return
+	end
+
+	if not alive(player_unit) or player_unit:character_damage():need_revive() or player_unit:character_damage():dead() then
+		return
+	end
+
+	if not alive(unit) or not unit:base() or not damage_info then
+		return
+	end
+
+	local data = tweak_data.upgrades.damage_to_hot_data
+
+	if not data then
+		return
+	end
+
+	if self._next_allowed_doh_t and t < self._next_allowed_doh_t then
+		return
+	end
+
+	local add_stack_sources = data.add_stack_sources or {}
+
+	if not add_stack_sources.swat_van and unit:base().sentry_gun then
+		return
+	elseif not add_stack_sources.civilian and CopDamage.is_civilian(unit:base()._tweak_table) then
+		return
+	end
+
+	if damage_info.attacker_unit:base().sentry_gun and not add_stack_sources.sentry_gun then
+		return
+	end
+	
+	if not add_stack_sources[damage_info.variant] then
+		return
+	end
+
+	if not unit:brain():is_hostile() then
+		return
+	end
+
+	local player_armor = managers.blackmarket:equipped_armor(data.works_with_armor_kit, true)
+
+	if not table.contains(data.armors_allowed or {}, player_armor) then
+		return
+	end
+
+	player_unit:character_damage():add_damage_to_hot()
+
+	self._next_allowed_doh_t = t + data.stacking_cooldown
+end
+
 -- Tag Team: tagged player will hear activation sound
 Hooks:PostHook(PlayerManager, "sync_tag_team", "sync_tag_team_sound_effect", function(self, tagged, owner, end_time)
 	if tagged == self:local_player() then
