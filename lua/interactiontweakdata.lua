5a2eb8d0434ef966258b3dc5a8655459d17ce492
hitscanner43 <hitscanner43@users.noreply.github.com>
2025-11-07 22:25:45 +0000

    Code formatting

--- lua/interactiontweakdata.lua
+++ lua/interactiontweakdata.lua
@@ -6,7 +6,7 @@ Hooks:PostHook(InteractionTweakData, "init", "eclipse_init", function(self)
 	self.gage_assignment.timer = 0
 	self.hostage_move.timer = self.hostage_move.timer / 2
 	self.hostage_stay.timer = 0
-	
+
 	self.hostage_trade.contour_preset = "hostage_trade_uncustody"
 	self.hostage_trade.contour_flash_interval = 0.5
 	self.hostage_trade_resources = deep_clone(self.hostage_trade)
