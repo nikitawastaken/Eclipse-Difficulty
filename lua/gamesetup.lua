440d96a6c6e0acb11ea009ba8fba1dd8d7ed52d4
moon <108492539+erushinj@users.noreply.github.com>
2025-12-04 14:23:21 -0600

    Simplify custom package loading in gamesetup

--- lua/gamesetup.lua
+++ lua/gamesetup.lua
@@ -1,126 +1,75 @@
-local level_id = Eclipse.utils.level_id()
-
 Hooks:PostHook(GameSetup, "load_packages", "eclipse_load_packages", function(self)
-	local fbi_heists = {
-		["watchdogs_1"] = true,
-		["watchdogs_1_night"] = true,
-		["watchdogs_2"] = true,
-		["watchdogs_2_day"] = true,
-		["firestarter_1"] = true,
-		["firestarter_2"] = true,
-		["firestarter_3"] = true,
-		["alex_3"] = true,
-		["hox_2"] = true,
-		--	["hox_3"] = true, -- They're already loaded here
-		["man"] = true,
+	local level_id = Eclipse.utils.level_id()
+	if not level_id then
+		return
+	end
+
+	local custom_packages = {
+		["packages/security_mcmansion"] = {
+			["watchdogs_1"] = true,
+			["watchdogs_1_night"] = true,
+			["watchdogs_2"] = true,
+			["watchdogs_2_day"] = true,
+			["firestarter_1"] = true,
+			["firestarter_2"] = true,
+			["firestarter_3"] = true,
+			["alex_3"] = true,
+			["hox_2"] = true,
+			-- ["hox_3"] = true, -- They're already loaded here
+			["man"] = true,
+		},
+		["packages/female_bikers"] = {
+			["welcome_to_the_jungle_1"] = true,
+			["welcome_to_the_jungle_1_night"] = true,
+			["cane"] = true,
+			["mex"] = true,
+		},
+		["packages/murkywater"] = {
+			["brb"] = true,
+		},
+		["packages/us_army"] = {
+			["arm_for"] = true,
+			["roberts"] = true,
+			["crojob2"] = true,
+			["crojob3"] = true,
+			["jolly"] = true,
+			["peta2"] = true,
+			["trai"] = true,
+		},
+		["packages/gensec_tactical_security"] = {
+			["dah"] = true,
+			["arena"] = true,
+		},
+		["packages/lapd"] = {
+			-- ["rvd1"] = true, -- They're already loaded here
+			-- ["rvd2"] = true, -- They're already loaded here
+			["kenaz"] = true,
+			["jolly"] = true,
+			["pal"] = true,
+			["friend"] = true,
+		},
+		["packages/coast_guard"] = {
+			["chca"] = true,
+			["deep"] = true,
+		},
+		["packages/texas_rangers"] = {
+			["dinner"] = true,
+			-- ["ranc"] = true, -- They're already loaded here
+			["trai"] = true,
+			-- ["corp"] = true, -- They're already loaded here
+		},
+		["packages/security_deep"] = {
+			["ranc"] = true,
+			["corp"] = true,
+			-- ["deep"] = true, -- They're already loaded here
+		},
 	}
 
-	local female_bikers = {
-		["welcome_to_the_jungle_1"] = true,
-		["welcome_to_the_jungle_1_night"] = true,
-		["cane"] = true,
-		["mex"] = true,
-	}
-
-	local murkies = {
-		["brb"] = true,
-	}
-
-	local gensec_tac_teams = {
-		["dah"] = true,
-		["arena"] = true,
-	}
-
-	local us_army = {
-		["arm_for"] = true,
-		["roberts"] = true,
-		["crojob2"] = true,
-		["crojob3"] = true,
-		["jolly"] = true,
-		["peta2"] = true,
-		["trai"] = true,
-	}
-
-	local lapd = {
-		--	["rvd1"] = true, -- They're already loaded here
-		--	["rvd2"] = true,
-		["kenaz"] = true,
-		["jolly"] = true,
-		["pal"] = true,
-		["friend"] = true,
-	}
-
-	local coast_guard = {
-		["chca"] = true,
-		["deep"] = true,
-	}
-
-	local texas_rangers = {
-		["dinner"] = true,
-		--	["ranc"] = true, -- They're already loaded here
-		["trai"] = true,
-		--	["corp"] = true,
-	}
-
-	local security_deep = {
-		["ranc"] = true,
-		["corp"] = true,
-		--	["deep"] = true, -- They're already loaded here
-	}
-
-	if level_id then
-		if fbi_heists[level_id] and not PackageManager:loaded("packages/security_mcmansion") then
-			Eclipse:log_console("Loading FBI security package...")
-			table.insert(self._loaded_diff_packages, "packages/security_mcmansion")
-			PackageManager:load("packages/security_mcmansion")
-		end
-
-		if female_bikers[level_id] and not PackageManager:loaded("packages/female_bikers") then
-			Eclipse:log_console("Loading female biker package...")
-			table.insert(self._loaded_diff_packages, "packages/female_bikers")
-			PackageManager:load("packages/female_bikers")
-		end
-
-		if murkies[level_id] and not PackageManager:loaded("packages/murkywater") then
-			Eclipse:log_console("Loading Murkywater package...")
-			table.insert(self._loaded_diff_packages, "packages/murkywater")
-			PackageManager:load("packages/murkywater")
-		end
-
-		if us_army[level_id] and not PackageManager:loaded("packages/us_army") then
-			Eclipse:log_console("Loading US army package...")
-			table.insert(self._loaded_diff_packages, "packages/us_army")
-			PackageManager:load("packages/us_army")
-		end
-
-		if gensec_tac_teams[level_id] and not PackageManager:loaded("packages/gensec_tactical_security") then
-			Eclipse:log_console("Loading GenSec tac teams package...")
-			table.insert(self._loaded_diff_packages, "packages/gensec_tactical_security")
-			PackageManager:load("packages/gensec_tactical_security")
-		end
-
-		if lapd[level_id] and not PackageManager:loaded("packages/lapd") then
-			Eclipse:log_console("Loading LAPD package...")
-			table.insert(self._loaded_diff_packages, "packages/lapd")
-			PackageManager:load("packages/lapd")
-		end
-
-		if coast_guard[level_id] and not PackageManager:loaded("packages/coast_guard") then
-			Eclipse:log_console("Loading Coast Guard package...")
-			table.insert(self._loaded_diff_packages, "packages/coast_guard")
-			PackageManager:load("packages/coast_guard")
-		end
-
-		if texas_rangers[level_id] and not PackageManager:loaded("packages/texas_rangers") then
-			Eclipse:log_console("Loading Texas Rangers package...")
-			table.insert(self._loaded_diff_packages, "packages/texas_rangers")
-			PackageManager:load("packages/texas_rangers")
-		end
-
-		if security_deep[level_id] and not PackageManager:loaded("packages/security_deep") then
-			Eclipse:log_console("Loading Bellmead Security package...")
-			table.insert(self._loaded_diff_packages, "packages/security_deep")
-			PackageManager:load("packages/security_deep")
+	for package_id, package_levels in pairs(custom_packages) do
+		if package_levels[level_id] and not PackageManager:loaded(package_id) then
+			Eclipse:log_console(string.format('Loading custom package "%s"...', package_id))
+			table.insert(self._loaded_diff_packages, package_id)
+			PackageManager:load(package_id)
 		end
 	end
 end)
