e83dbbdb9c79f80afa577007bec89dd6f79d6819
hitscanner <121458955+hitscanner43@users.noreply.github.com>
2025-12-05 15:16:37 +0100

    Update "clear" voice line implementation

--- lua/groupaitweakdata.lua
+++ lua/groupaitweakdata.lua
@@ -133,6 +133,8 @@ Hooks:PostHook(GroupAITweakData, "_init_chatter_data", "sh__init_chatter_data", 
 	self.enemy_chatter.aggressive.duration = duration_medium
 	self.enemy_chatter.contact.duration = duration_medium
 	self.enemy_chatter.contact.radius = radius_medium
+	self.enemy_chatter.clear.duration = duration_medium
+	self.enemy_chatter.clear.radius = radius_medium
 	self.enemy_chatter.retreat.queue = "m01"
 	self.enemy_chatter.push = clone(self.enemy_chatter.go_go)
 	self.enemy_chatter.push.queue = "pus"
@@ -142,8 +144,6 @@ Hooks:PostHook(GroupAITweakData, "_init_chatter_data", "sh__init_chatter_data", 
 	self.enemy_chatter.flank.queue = "t01"
 	self.enemy_chatter.open_fire = clone(self.enemy_chatter.go_go)
 	self.enemy_chatter.open_fire.queue = "att"
-	self.enemy_chatter.clear = clone(self.enemy_chatter.go_go)
-	self.enemy_chatter.clear.queue = "clr"
 	self.enemy_chatter.suppress = clone(self.enemy_chatter.go_go)
 	self.enemy_chatter.suppress.queue = "hlp"
 	self.enemy_chatter.get_hostages = clone(self.enemy_chatter.go_go)
