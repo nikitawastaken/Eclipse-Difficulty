3b486b27a763400a39f2526ca988413baf306172
hitscanner <1my3mail3@gmail.com>
2025-12-28 20:10:40 +0100

    Fix increased zipline cost on bex not applying

--- lua/preplanningtweakdata.lua
+++ lua/preplanningtweakdata.lua
@@ -1,8 +1,9 @@
-local level_id = Eclipse.utils.level_id()
-
-local expensive_ilija_heists = table.list_to_set({
+PrePlanningTweakData.expensive_ilija_heists = table.list_to_set({
 	"trai",
 })
+
+local level_id = Eclipse.utils.level_id()
+local is_bex = level_id == "bex" 
 
 Hooks:PostHook(PrePlanningTweakData, "init", "eclipse_init", function(self)
 	-- less trivial big bank preplan
@@ -11,13 +12,10 @@ Hooks:PostHook(PrePlanningTweakData, "init", "eclipse_init", function(self)
 	self.types.escape_elevator_loud.budget_cost = 6
 	self.types.escape_bus_loud.budget_cost = 10
 
-	-- Bexico
+	-- Increase Ilija cost 
+	self.types.sniper.budget_cost = self.expensive_ilija_heists[level_id] and 6 or 4
+	
+	-- Tweak San Martin preplanning costs
 	self.types.bex_car_pull.budget_cost = 8
-	self.types.bex_zipline.budget_cost = 4
-
-	if expensive_ilija_heists[level_id] then
-		self.types.sniper.budget_cost = 6
-	else
-		self.types.sniper.budget_cost = 4
-	end
+	self.types.bag_zipline.budget_cost = is_bex and 4 or self.types.bag_zipline.budget_cost
 end)
