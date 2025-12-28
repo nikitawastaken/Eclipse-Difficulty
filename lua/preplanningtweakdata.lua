8c9b61a90fd06d44346140ce861e4f1e22ba70df
hitscanner43 <hitscanner43@users.noreply.github.com>
2025-12-28 19:11:27 +0000

    Code formatting

--- lua/preplanningtweakdata.lua
+++ lua/preplanningtweakdata.lua
@@ -3,7 +3,7 @@ PrePlanningTweakData.expensive_ilija_heists = table.list_to_set({
 })
 
 local level_id = Eclipse.utils.level_id()
-local is_bex = level_id == "bex" 
+local is_bex = level_id == "bex"
 
 Hooks:PostHook(PrePlanningTweakData, "init", "eclipse_init", function(self)
 	-- less trivial big bank preplan
@@ -12,9 +12,9 @@ Hooks:PostHook(PrePlanningTweakData, "init", "eclipse_init", function(self)
 	self.types.escape_elevator_loud.budget_cost = 6
 	self.types.escape_bus_loud.budget_cost = 10
 
-	-- Increase Ilija cost 
+	-- Increase Ilija cost
 	self.types.sniper.budget_cost = self.expensive_ilija_heists[level_id] and 6 or 4
-	
+
 	-- Tweak San Martin preplanning costs
 	self.types.bex_car_pull.budget_cost = 8
 	self.types.bag_zipline.budget_cost = is_bex and 4 or self.types.bag_zipline.budget_cost
