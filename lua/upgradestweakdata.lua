6c99c010bab5cc6946a7faf99d49d06380ac4e91
hitscanner <1my3mail3@gmail.com>
2026-01-06 19:01:20 +0100

    Decrease armour move speed penalties

--- lua/upgradestweakdata.lua
+++ lua/upgradestweakdata.lua
@@ -17,7 +17,7 @@ function UpgradesTweakData:_init_pd2_values(tweak_data)
 
 	self.values.player.body_armor = {
 		armor = { 0, 2, 4.5, 7, 10, 14, 18 },
-		movement = { 1, 0.925, 0.85, 0.775, 0.7, 0.625, 0.55 },
+		movement = { 1, 0.95, 0.9, 0.85, 0.775, 0.7, 0.625 },
 		concealment = { 30, 26, 23, 21, 18, 12, 1 },
 		dodge = { 0.1, 0.05, 0, -0.05, -0.2, -0.25, -0.55 },
 		damage_shake = { 1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4 },
