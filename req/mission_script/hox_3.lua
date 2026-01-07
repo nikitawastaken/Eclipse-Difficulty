1afbc84cf696f44d9318517e09590d9f580bbcf5
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2026-01-07 18:30:47 +0100

    aa

--- req/mission_script/hox_3.lua
+++ req/mission_script/hox_3.lua
@@ -54,6 +54,12 @@ local van_spawn = {
 local scripted_swat_van_spawn = {
 	groups = preferred.no_cops_agents_hrt_cloakers_snipers,
 }
+local cloaker_spawn = {
+	values = {
+		interval = 90,
+	},
+	groups = preferred.only_cloakers_single,
+}
 return {
 	[101735] = {
 		ponr = {
@@ -165,6 +171,14 @@ return {
 	[102424] = flank_spawn,
 	[102438] = flank_spawn,
 	[102459] = flank_spawn,
+	[400074] = cloaker_spawn,
+	[400075] = cloaker_spawn,
+	[400076] = cloaker_spawn,
+	[400077] = cloaker_spawn,
+	[400078] = cloaker_spawn,
+	[400079] = cloaker_spawn,
+	[400080] = cloaker_spawn,
+	[400081] = cloaker_spawn,
 	-- Harassers
 	[100883] = harasser,
 	[100884] = harasser,
