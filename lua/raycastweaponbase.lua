810a2f7ca669b789762dd0ff33f9c16a2b63711a
hitscanner <1my3mail3@gmail.com>
2026-01-18 18:47:53 +0100

    Spread fixes

--- lua/raycastweaponbase.lua
+++ lua/raycastweaponbase.lua
@@ -49,6 +49,26 @@ function RaycastWeaponBase:is_explosive()
 	return self._ammo_data and self._ammo_data.explosive_ammo or self:is_category("grenade_launcher") or false
 end
 
+-- Only use standing stance spread, the rest is now handled by stance multipliers
+function RaycastWeaponBase:_get_spread(user_unit)
+	local spread_multiplier = self:spread_multiplier()
+	local current_state = user_unit:movement()._current_state
+
+	if current_state._moving then
+		for _, category in ipairs(self:weapon_tweak_data().categories) do
+			spread_multiplier = spread_multiplier * managers.player:upgrade_value(category, "move_spread_multiplier", 1)
+		end
+	end
+
+	if not current_state:in_steelsight() then
+		for _, category in ipairs(self:weapon_tweak_data().categories) do
+			spread_multiplier = spread_multiplier * managers.player:upgrade_value(category, "hip_fire_spread_multiplier", 1)
+		end
+	end
+	
+	return self._spread * tweak_data.weapon[self._name_id].spread["standing"] * spread_multiplier
+end
+
 local mvec_to = Vector3()
 local mvec_right_ax = Vector3()
 local mvec_up_ay = Vector3()
@@ -70,19 +90,19 @@ function RaycastWeaponBase:_fire_raycast(user_unit, from_pos, direction, dmg_mul
 	spread_y = spread_y or spread_x
 	spread_mul = spread_mul or 1
 
-	spread_x = math.max(spread_x, 0)
-	spread_y = math.max(spread_y, 0)
-
 	mvector3.cross(mvec_right_ax, direction, math.UP)
 	mvec3_norm(mvec_right_ax)
 	mvector3.cross(mvec_up_ay, direction, mvec_right_ax)
 	mvec3_norm(mvec_up_ay)
 	mvec3_set(mvec_spread_direction, direction)
 
+	local r = math.random()
 	local theta = math.random() * 360
+	spread_x = math.max(math.min(spread_x * spread_mul, 90), -90)
+	spread_y = math.max(math.min(spread_y * spread_mul, 90), -90)
 
-	mvec3_mul(mvec_right_ax, math.rad(math.sin(theta) * math.random() * spread_x * spread_mul))
-	mvec3_mul(mvec_up_ay, math.rad(math.cos(theta) * math.random() * spread_y * spread_mul))
+	mvec3_mul(mvec_right_ax,  math.cos(theta) * math.tan(r * spread_x))
+	mvec3_mul(mvec_up_ay, -1 * math.sin(theta) * math.tan(r * spread_y))
 	mvec3_add(mvec_spread_direction, mvec_right_ax)
 	mvec3_add(mvec_spread_direction, mvec_up_ay)
 	mvec3_set(mvec_to, mvec_spread_direction)
