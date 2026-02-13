c8fcedc5556a7cdb2df26feff01b44ad850cb08b
SonicSoapyBoi <SonicSoapyBoi@users.noreply.github.com>
2026-02-13 21:36:24 +0000

    Code formatting

--- req/packages/murky_mercs_scripted.lua
+++ req/packages/murky_mercs_scripted.lua
@@ -1,6 +1,6 @@
 ---@module Murkywater Mercenaries (Scripted) Package
 local M = {
-    "units/payday2/characters/ene_murkywater_1/ene_murkywater_1",
+	"units/payday2/characters/ene_murkywater_1/ene_murkywater_1",
 	"units/payday2/characters/ene_murkywater_2/ene_murkywater_2",
 }
 
