53b56e7cdd6f4b3c19d30a36c2c53ca26bb730f1
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2026-02-13 22:35:34 +0100

    bring back murkies to brooklyn bank

---
+++ req/packages/murky_mercs_scripted.lua
@@ -0,0 +1,7 @@
+---@module Murkywater Mercenaries (Scripted) Package
+local M = {
+    "units/payday2/characters/ene_murkywater_1/ene_murkywater_1",
+	"units/payday2/characters/ene_murkywater_2/ene_murkywater_2",
+}
+
+return M
