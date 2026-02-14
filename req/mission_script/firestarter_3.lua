e0c033f504edc50ce89fb4f3b58500845579d7da
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2026-02-14 19:34:46 +0100

    fix infinite dozer spawn bug in fs day 3

--- req/mission_script/firestarter_3.lua
+++ req/mission_script/firestarter_3.lua
@@ -185,10 +185,11 @@ return {
 			{ id = 105648, remove = true },
 		},
 	},
-	-- trigger on end assault
+	-- trigger swat choppers on end assault
 	[101304] = {
 		on_executed = {
-			{ id = 400075, delay = 10 },
+			{ id = 400080, delay = 10 },
+			{ id = 400081, delay = 10 },
 		},
 	},
 	-- disable the dozer chopper event if the heli1 gas event has been triggered
@@ -213,8 +214,8 @@ return {
 	[100438] = {
 		on_executed = {
 			{ id = 103540, delay = 0 },
-			{ id = 400077, delay = 0 },
-			{ id = 400078, delay = 0 },
+			{ id = 400080, delay = 0 },
+			{ id = 400081, delay = 0 },
 		},
 	},
 	-- enable spawns sooner
