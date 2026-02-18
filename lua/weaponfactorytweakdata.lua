d7e887da17860271ad0bec1d031894dfa9e12a17
hitscanner43 <hitscanner43@users.noreply.github.com>
2026-02-18 16:20:12 +0000

    Code formatting

--- lua/weaponfactorytweakdata.lua
+++ lua/weaponfactorytweakdata.lua
@@ -1102,7 +1102,7 @@ Hooks:PostHook(WeaponFactoryTweakData, "init", "eclipse_init", function(self)
 
 	self.parts.wpn_fps_saw_body_silent.stats.suppression = 9
 	self.parts.wpn_fps_saw_body_silent.stats.alert_size = 9
-	
+
 	self.parts.wpn_fps_saw_body_speed.stats.damage = 0
 	self.parts.wpn_fps_saw_body_speed.stats.concealment = -2
 	self.parts.wpn_fps_saw_body_speed.custom_stats = { fire_rate_multiplier = 1.5 }
