4e29c6790a61433462c393a80d4802c6d9d96931
hitscanner <121458955+hitscanner43@users.noreply.github.com>
2026-02-26 15:52:30 +0100

    Update pines.lua

--- req/mission_script/pines.lua
+++ req/mission_script/pines.lua
@@ -4,6 +4,36 @@ local disabled = {
 	},
 }
 return {
+	-- Add new reinforce
+	[100109] = { -- Police
+		reinforce = {
+			{
+				name = "lumber",
+				force = 2,
+				position = Vector3(-8100, 14500, 4255),
+			},
+			{
+				name = "zipline",
+				force = 2,
+				position = Vector3(-10200, 9800, 3700),
+			},
+			{
+				name = "wreckage",
+				force = 2,
+				position = Vector3(-3100, 10150, 2975),
+			},
+			{
+				name = "logging_machine",
+				force = 2,
+				position = Vector3(-8895, 8000, 3115),
+			},
+			{
+				name = "cliff",
+				force = 2,
+				position = Vector3(-11675, 11550, 4150),
+			},
+		},
+	},
 	-- disable endless assault (the heist doesn't need one)
 	[101473] = disabled,
 }
