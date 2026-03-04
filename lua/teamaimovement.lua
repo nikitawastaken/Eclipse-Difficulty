91b8e2f80d264d55ba527fdc5e97448acbd2994c
hitscanner <121458955+hitscanner43@users.noreply.github.com>
2026-03-04 18:35:12 +0100

    SH ketchup

--- lua/teamaimovement.lua
+++ lua/teamaimovement.lua
@@ -14,16 +14,6 @@ setmetatable(HuskPlayerMovement.reload_times, {
 	end,
 })
 
--- Apply default carry speed upgrade to bots
-function TeamAIMovement:set_carry_speed_modifier(...)
-	TeamAIMovement.super.set_carry_speed_modifier(self, ...)
-
-	if self._carry_speed_modifier then
-		local carry_upgrade = managers.player:upgrade_value("carry", "movement_speed_multiplier", 1)
-		self._carry_speed_modifier = math.clamp(self._carry_speed_modifier * carry_upgrade, 0, 1)
-	end
-end
-
 -- Properly load secondary weapons from factory IDs
 function TeamAIMovement:add_weapons()
 	if Network:is_server() then
