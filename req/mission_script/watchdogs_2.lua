39e12be68c2cfe01d235eff00b12d50e35616ef8
moon <108492539+erushinj@users.noreply.github.com>
2026-03-10 04:05:30 -0600

    Fix Watchdogs day 2 scripted spawn swaps
    - No longer spawns SWAT Shields on DW non-PJ

--- req/mission_script/watchdogs_2.lua
+++ req/mission_script/watchdogs_2.lua
@@ -42,14 +42,10 @@ local no_participate_to_group_ai = {
 	},
 }
 local blockade_enemy1 = {
-	values = {
-		enemy = is_eclipse and elite_bulldozer or bulldozer,
-	},
+	enemy = is_eclipse and elite_bulldozer or bulldozer,
 }
 local blockade_enemy2 = {
-	values = {
-		enemy = is_eclipse_pro and elite_shield or shield,
-	},
+	enemy = is_eclipse_pro and elite_shield or shield,
 }
 local heli_enemy1 = {
 	values = {
