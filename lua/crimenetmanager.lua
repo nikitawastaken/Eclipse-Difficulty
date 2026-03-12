2ebca36e0f086c66b9f628e8983940c4d1a82974
Hater-mater <swetlaniki@gmail.com>
2026-03-12 11:06:57 +0300

    Fix difficulty filter abuse
    Difficulty filter no longer affect on difficulty of random heists popup

--- lua/crimenetmanager.lua
+++ lua/crimenetmanager.lua
@@ -15,3 +15,83 @@ function CrimeNetGui:_create_job_gui(data, type, fixed_x, fixed_y, fixed_locatio
 
 	return gui_data
 end
+
+-- Fix difficulty filter abuse
+function CrimeNetManager:_setup()
+	if self._presets then
+		return
+	end
+
+	self._presets = {}
+	local plvl = managers.experience:current_level()
+	local player_stars = math.clamp(math.ceil((plvl + 1) / 10), 1, 10)
+	local stars = player_stars
+	local jc = math.lerp(0, 100, stars / 10)
+	local jcs = tweak_data.narrative.STARS[stars].jcs
+	local no_jcs = #jcs
+	local chance_curve = tweak_data.narrative.STARS_CURVES[player_stars]
+	local start_chance = tweak_data.narrative.JC_CHANCE
+	local jobs_by_jc = self:_get_jobs_by_jc()
+	local no_picks = self:_number_of_jobs(jcs, jobs_by_jc)
+	local j = 0
+	local tests = 0
+
+	while j < no_picks do
+		for i = 1, no_jcs do
+			local chance = nil
+
+			if no_jcs - 1 == 0 then
+				chance = 1
+			else
+				chance = math.lerp(start_chance, 1, math.pow((i - 1) / (no_jcs - 1), chance_curve))
+			end
+
+			if not jobs_by_jc[jcs[i]] then
+				-- Nothing
+			elseif #jobs_by_jc[jcs[i]] ~= 0 then
+				local job_data = nil
+
+				if self._debug_mass_spawning then
+					job_data = jobs_by_jc[jcs[i]][math.random(#jobs_by_jc[jcs[i]])]
+				else
+					job_data = table.remove(jobs_by_jc[jcs[i]], math.random(#jobs_by_jc[jcs[i]]))
+				end
+
+				local job_tweak = tweak_data.narrative:job_data(job_data.job_id)
+				local chance_multiplier = job_tweak and job_tweak.spawn_chance_multiplier or 1
+				job_data.chance = chance * chance_multiplier
+
+				table.insert(self._presets, job_data)
+
+				j = j + 1
+
+				break
+			end
+		end
+
+		tests = tests + 1
+
+		if self._debug_mass_spawning then
+			if tweak_data.gui.crime_net.debug_options.mass_spawn_limit <= tests then
+				break
+			end
+		elseif no_picks <= tests then
+			break
+		end
+	end
+
+	local old_presets = self._presets
+	self._presets = {}
+
+	while #old_presets > 0 do
+		table.insert(self._presets, table.remove(old_presets, math.random(#old_presets)))
+	end
+end
+
+function CrimeNetManager:update_difficulty_filter()
+	if self._presets then
+		self._presets = nil
+
+		self:_setup()
+	end
+end
