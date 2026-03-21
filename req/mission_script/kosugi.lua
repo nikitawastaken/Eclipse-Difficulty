e3a3603acfd04ad7dc998efe7e149dec5932e69f
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2026-03-21 20:31:19 +0100

    Samurai found alive in the vault (bit EVIL kosugi)

--- req/mission_script/kosugi.lua
+++ req/mission_script/kosugi.lua
@@ -1,19 +1,48 @@
 local is_pro_job = Eclipse.utils.is_pro_job()
 local normal, hard, eclipse = Eclipse.utils.diff_groups()
+local is_eclipse = Eclipse.utils.is_eclipse()
 local scripted_enemy = Eclipse.scripted_enemy
-local jerome_camera_man = scripted_enemy.security_3
+local jerome_man = scripted_enemy.security_3
 local bags_required = {
 	values = {
 		amount = (normal and 4 or hard and 8 or 12) + (is_pro_job and 4 or 0),
 	},
 }
-local surv_man = {
-	enemy = jerome_camera_man,
+local disabled = {
+	values = {
+		enabled = false,
+	},
 }
+local murky_choppers = {
+	values = {
+		amount = is_eclipse and 2 or 1,
+		amount_random = (is_pro_job and not is_eclipse) and 1 or 0,
+	},
+}
+local surv_keycard_man = {
+	enemy = jerome_man,
+}
+
 
 return {
 	-- replace regular murkywater with shotgun guard in the surv room
-	[100711] = surv_man,
+	[100711] = surv_keycard_man,
+	-- change murkywater's model to regular shotgun guard to better indicate who has the keycard
+	[101076] = surv_keycard_man,
+	[101266] = surv_keycard_man,
+	[101272] = surv_keycard_man,
+	[101279] = surv_keycard_man,
+	-- disable chopper exploit (only for the chopper that deploys murkies :dajoker:)
+	[104603] = disabled,
+	[104604] = disabled,
+	-- 2 choppers can get deployed on Death Wish :dwpj: (with a random chance of 2 choppers on Pro Jobs below Death Wish)
+	[101384] = murky_choppers,
+	-- the same chopper that deploys murkies can now return :dajoker:
+	[100340] = {
+		on_executed = {
+			{ id = 100303, delay = (is_eclipse and 300 or 360) - (is_pro_job and 60 or 0) },
+		},
+	},
 	-- bag tweaks (4 on easy and normal, 8 on hard and overkill and 12 on Death Wish)
 	-- 4 more bags to secure on pro jobs
 	[101156] = bags_required,
