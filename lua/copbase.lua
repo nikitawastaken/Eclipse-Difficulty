2684b13384964b5f6e18258101e47b744a8f876e
hitscanner43 <hitscanner43@users.noreply.github.com>
2026-03-31 21:25:43 +0000

    Code formatting

--- lua/copbase.lua
+++ lua/copbase.lua
@@ -1,5 +1,5 @@
 Month = os.date("%m")
-Day = os.date("%d")	
+Day = os.date("%d")
 
 local mat_vars = Eclipse:require("unit_material_vars")
 local mat_var_paths = table.list_to_set(mat_vars)
@@ -120,7 +120,7 @@ function CopBase:_run_unit_sequences()
 
 		local head_material = sequence_head and sequence_head.material
 		local head_sequences = sequence_head and sequence_head.run_sequence
-		
+
 		-- If the unit had a head defined in its .unit file, spawn and parent it
 		if spawn_manager_ext then
 			if head then
@@ -153,7 +153,7 @@ function CopBase:_run_unit_sequences()
 						self._head_unit:damage():run_sequence_simple("set_jerome_mode")
 					end
 				end
-				
+
 				for _, sequence in pairs(head_sequences) do
 					if self._head_unit:damage():has_sequence(sequence) then
 						self._head_unit:damage():run_sequence_simple(sequence)
