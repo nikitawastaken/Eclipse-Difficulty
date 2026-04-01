6cd9d30487c8e41d6119fcf9ea269fa7dde24676
hitscanner <1my3mail3@gmail.com>
2026-04-01 12:54:01 +0200

    Slightly slow down scripted dozer spam on Undercover

--- req/mission_script_add/man.lua
+++ req/mission_script_add/man.lua
@@ -9,7 +9,7 @@ local is_eclipse = Eclipse.utils.is_eclipse()
 local is_pro_job = Eclipse.utils.is_pro_job()
 local is_eclipse_pro = Eclipse.utils.is_eclipse_pro()
 local dozer_random_amount = overkill_and_above and 2 or 1
-local dozers_respawn = (is_eclipse and 240 or 300) - (is_eclipse_pro and 60 or is_pro_job and 30 or 0)
+local dozers_respawn = (is_eclipse and 300 or 360) - (is_pro_job and 60 or 0)
 local dozer_event = not normal or (is_pro_job and normal) and true or false
 
 local cloaker = scripted_enemy.cloaker
