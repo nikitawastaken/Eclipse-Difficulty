2ce3119dc2d43597cdca7ef20f95c8abce5dbeb5
Hater-mater <swetlaniki@gmail.com>
2026-04-17 01:12:20 +0300

    Taser nades now FF tase teammates
    * Removed vanilla check that only apply to owner
    * You can't tase team AI now

--- lua/electricgrenade.lua
+++ lua/electricgrenade.lua
@@ -62,7 +62,7 @@ function ElectricGrenade:_detonate(tag, unit, body, other_unit, other_body, posi
 	local pos = self._unit:position()
 	local normal = math.UP
 	local range = self._range
-	local slot_mask = managers.slot:get_mask("explosion_targets")
+	local slot_mask = managers.slot:get_mask("explosion_targets") - managers.slot:get_mask("all_criminals")
 
 	managers.explosion:play_sound_and_effects(pos, normal, range, self._custom_params)
 
@@ -108,7 +108,7 @@ end
 function ElectricGrenade:_tase_player()
 	local player = managers.player:player_unit()
 
-	if alive(player) and player == self:thrower_unit() and player:character_damage().on_self_tased then
+	if alive(player) then
 		local detonate_pos = self._unit:position() + math.UP * 100
 		local range = self._range
 		local affected, line_of_sight, travel_dis, linear_dis = QuickFlashGrenade._chk_dazzle_local_player(self, detonate_pos, range)
