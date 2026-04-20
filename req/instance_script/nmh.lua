8d4574747c4cdd162ecf3c1586af5df7a94451ed
moon <108492539+erushinj@users.noreply.github.com>
2026-04-19 21:12:15 -0600

    Merciful Centrifuges II: Electric Boogaloo

--- req/instance_script/nmh.lua
+++ req/instance_script/nmh.lua
@@ -10,26 +10,29 @@ local patches = {
 return {
 	["levels/instances/unique/nmh/nmh_fuge/world/world"] = function(result)
 		local centrifuge = patches.centrifuge
+		local chance_add_element = Eclipse.mission_elements.gen_chance_operator(140001, "chance_add", Vector3(0, 0, 0), Rotation(0, 0, 0), {
+			enabled = true,
+			chance = 5,
+			operation = "add_chance",
+			elements = { centrifuge.chance_element },
+		})
+		local chance_reset_element = Eclipse.mission_elements.gen_chance_operator(140002, "chance_reset", Vector3(0, 0, 0), Rotation(0, 0, 0), {
+			enabled = true,
+			operation = "reset_chance",
+			elements = { centrifuge.chance_element },
+		})
 		for _, element in ipairs(result.default.elements) do
 			if element.id == centrifuge.interact_with_centrifuge then
 				element.values.trigger_times = 0 -- why it has set trigger times to 9?
 			elseif element.id == centrifuge.chance_fail then
-				element.values.callback = function()
-					local chance_element = managers.mission:get_element_by_id(centrifuge.chance_element)
-					if chance_element and chance_element.chance_operation_add_chance then
-						chance_element:chance_operation_add_chance(5)
-					end
-				end
+				table.insert(element.values.on_executed, { id = chance_add_element.id, delay = 0 })
 			elseif element.id == centrifuge.chance_success then
-				element.values.callback = function()
-					local chance_element = managers.mission:get_element_by_id(centrifuge.chance_element)
-					if chance_element and chance_element.chance_operation_reset then
-						chance_element:chance_operation_reset()
-					end
-				end
+				table.insert(element.values.on_executed, { id = chance_reset_element.id, delay = 0 })
 			elseif element.id == centrifuge.chance_element then
 				element.values.chance = 25 -- Vanilla is 28
 			end
 		end
+		table.insert(result.default.elements, chance_add_element)
+		table.insert(result.default.elements, chance_reset_element)
 	end,
 }
