e8fe6fdf2a133ddec561730537e6e9e67ae274c8
hitscanner <1my3mail3@gmail.com>
2026-04-25 10:41:18 +0200

    Loot dropoff point reinforce

--- lua/groupaitweakdata.lua
+++ lua/groupaitweakdata.lua
@@ -3753,6 +3753,7 @@ Hooks:PostHook(GroupAITweakData, "_init_task_data", "eclipse__init_task_data", f
 	self.besiege.reenforce.interval = { 10, 20, 30 }
 	self.undershot_reenforce_interval_factor = 0.9
 	self.init_reenforce_delay = 15
+	self.use_loot_drop_reenforce = true
 	self.use_equipment_reenforce = true
 	self.equipment_reenforce = table.list_to_set({
 		"doctor_bag",
