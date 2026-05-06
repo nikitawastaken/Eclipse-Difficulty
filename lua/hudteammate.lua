c3ff1956f10632a943375f8a2a966f49cdcaa332
Hater-mater <Hater-mater@users.noreply.github.com>
2026-05-06 19:11:54 +0000

    Code formatting

--- lua/hudteammate.lua
+++ lua/hudteammate.lua
@@ -1,5 +1,5 @@
 -- No outlines mutator setting (hide teammate HUD panels)
-Hooks:PostHook(HUDTeammate, "add_panel", "add_panel_mutator_no_outlines", function (self)
+Hooks:PostHook(HUDTeammate, "add_panel", "add_panel_mutator_no_outlines", function(self)
 	if managers.mutators:modify_value("HUDTeammate:NoOutlines", false) then
 		local teammate_panel = self._panel
 		local main_player = self._main_player
