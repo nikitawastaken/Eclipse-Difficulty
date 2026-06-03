925c1ed14fc56953c319aec6755217844e498f2c
hitscanner <hitscanner43@users.noreply.github.com>
2026-06-03 21:00:14 +0200

    It's effect_spawner now :jerome:

--- req/envsmod/nightclub_03.lua
+++ req/envsmod/nightclub_03.lua
@@ -15,7 +15,7 @@ return {
 	environment_override = { -- File override
 		["environments/pd2_env_night/pd2_env_night"] = tostring(Eclipse.mod_path) .. "assets/environments/custom/nightclub_3.custom_xml",
 	},
-	particles = { -- Fog effects and such
+	effect_spawner = { -- Fog effects and such
 		["effects/payday2/environment/club_smoke_machine"] = {
 			{
 				position = Vector3(2779, -5599, 78),
