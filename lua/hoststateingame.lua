a42ab67a0d67c566861b68382297dfb2288ea567
rockymoto517 <rockymoto517@gmail.com>
2026-06-04 12:06:18 -0500

    host logs

--- lua/hoststateingame.lua
+++ lua/hoststateingame.lua
@@ -1,3 +1,4 @@
 Hooks:PostHook(HostStateInGame, "on_handshake_confirmation", "eclipse_post_handshake_confirm", function(self, _, peer)
-	peer:send("eclipse_sync_environment", Eclipse.current_environment)
+	Eclipse:log_chat("sending env sync request with env: " .. Eclipse.current_environment)
+	managers.network:session():send_to_peer(peer, "eclipse_sync_environment", Eclipse.current_environment)
 end)
