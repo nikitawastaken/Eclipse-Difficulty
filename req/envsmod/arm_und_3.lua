cf4f7a9c2790ef42e298b9388438be8a0b841ab9
Sokolov49 <81686591+Sokolov49@users.noreply.github.com>
2026-06-05 10:12:06 +0200

    frogg invades underpass

--- req/envsmod/arm_und_3.lua
+++ req/envsmod/arm_und_3.lua
@@ -16,5 +16,169 @@ return {
 	},
 	environment_override = { -- dark
 		["environments/pd2_env_foggy_bright/pd2_env_foggy_bright"] = tostring(Eclipse.mod_path) .. "assets/environments/custom/arm_und_dark.custom_xml",
-	} -- use in between fog blue
+	},
+	effect_spawner = {
+		["effects/envsmod/in_between/fog_very_faint_blue"] = {
+			{
+				position = Vector3(464, -704, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(464, -1942, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(464, 134, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(464, 1141, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(2124, 1141, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(2124, -2, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(3913, -2, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(3913, 1071, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(5566, 1071, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(5566, -34, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(6792, -34, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(6792, 1018, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(8157, 1018, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(8157, -93, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(8317, -828, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(8311, 1719, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(7582, 471, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(5156, 471, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(3561, 410, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(3873, 2578, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(3873, 5556, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(5885, -4478, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(5282, -5061, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(-568, -12, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(-902, 1088, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(-2882, 1088, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(-3182, -17, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(-3069, -1251, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(-3278, -2050, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(-2926, -3291, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(-3431, -4775, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(-3989, -2628, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(-4647, -1446, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(-6130, -1381, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(-5006, 34, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(-4499, 1183, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(-5821, 916, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(-6807, -95, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(-7945, 956, 265),
+				rotation = Rotation(0, 0, 0)
+			},
+			{
+				position = Vector3(-8896, 91, 265),
+				rotation = Rotation(0, 0, 0)
+			}
+		}
+	}
 }
