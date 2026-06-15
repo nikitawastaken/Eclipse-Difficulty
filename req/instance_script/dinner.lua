61ed48116ceb720c97b2bb9393b2df5b48dc04c7
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2026-06-15 18:32:37 +0200

    tweak dinner's dozer container instance

--- req/instance_script/dinner.lua
+++ req/instance_script/dinner.lua
@@ -2,9 +2,32 @@
 local M = {}
 
 local normal, hard, eclipse = Eclipse.utils.diff_groups()
-
+local scripted_enemy = Eclipse.scripted_enemy
 local chance_fewest_pigs = normal and 0 or hard and 0.01 or 0.02
 local fewest_pigs = math.random() < chance_fewest_pigs
+
+local random_dozers = {
+	scripted_enemy.bulldozer_1,
+	scripted_enemy.bulldozer_2,
+}
+local random_elite_dozers = {
+	scripted_enemy.elite_bulldozer_1,
+	scripted_enemy.elite_bulldozer_2,
+}
+
+local container_dozer = is_eclipse_pro and random_elite_dozers or random_dozers
+
+local filter_disable = Eclipse.utils.set_diff_groups("disable")
+local filter_hard_above = Eclipse.utils.set_diff_groups("hard_above")
+
+local patches = {
+	bulldozer_container_spawn = {
+		dozers = table.set(100007, 100009, 100008, 100010),
+		filters_disable = table.set(100002, 100003),
+		filters_hard_above = table.set(100004),
+		spawn_event_fix = table.set(100021),
+	},
+}
 
 M["levels/instances/unique/dinner/quad_meat/world/world"] = function(result)
 	for _, element in pairs(result.default.elements) do
@@ -51,6 +74,23 @@ M["levels/instances/unique/dinner/triple_meat/world/world"] = function(result)
 			else
 				-- nothing
 			end
+		end
+	end
+end
+M["levels/instances/unique/dinner/container_dozer_spawn/world/world"] = function(result)
+	local dozer_event = patches.bulldozer_container_spawn
+
+	for _, element in pairs(result.default.elements) do
+		local id = element.id
+
+		if dozer_event.dozers[id] then
+			element.values.enemy_table = container_dozer
+		elseif dozer_event.spawn_event_fix[id] then
+			element.values.event = "spawn"	
+		elseif dozer_event.filters_hard_above[id] then
+			table.map_append(element.values, filter_hard_above)
+		elseif dozer_event.filters_disable[id] then
+			table.map_append(element.values, filter_disable)
 		end
 	end
 end
