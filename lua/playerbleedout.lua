1241d8582a06f2d438da3a05967debb80805f68c
hitscanner <hitscanner43@users.noreply.github.com>
2026-06-20 18:36:58 +0200

    Fix a crash

--- lua/playerbleedout.lua
+++ lua/playerbleedout.lua
@@ -36,7 +36,7 @@ function PlayerBleedOut:on_rescue_SO_administered(revive_SO_data, receiver_unit,
 
 	on_rescue_SO_administered_original(self, revive_SO_data, receiver_unit, ...)
 
-	if had_bag and move_speed_modifier > 1 - UsefulBots.settings.drop_bag_percentage and not movement:carrying_bag() then
+	if had_bag and move_speed_modifier > 0.75 and not movement:carrying_bag() then
 		had_bag:carry_data():link_to(receiver_unit, false)
 		movement:set_carrying_bag(had_bag)
 	end
