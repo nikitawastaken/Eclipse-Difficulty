626759b21c5012b18022620fbc88d6d37b6d454a
hitscanner <hitscanner43@users.noreply.github.com>
2026-06-21 19:21:24 +0200

    Fix

--- lua/playertased.lua
+++ lua/playertased.lua
@@ -126,10 +126,10 @@ function PlayerTased:_register_revive_SO()
 	end
 
 	self._SO_id = "PlayerTased_assistance"
-	managers.groupai:state():add_special_objective(self._SO_id, Eclipse.utilts.team_ai_get_assist_SO(self._unit))
+	managers.groupai:state():add_special_objective(self._SO_id, Eclipse.utils.team_ai_get_assist_SO(self._unit))
 end
 
 Hooks:PostHook(PlayerTased, "exit", "exit_ub", function (self)
-	Eclipse.utilts.team_ai_stop_assist_objective(self._unit)
+	Eclipse.utils.team_ai_stop_assist_objective(self._unit)
 	self._SO_id = nil
 end)
