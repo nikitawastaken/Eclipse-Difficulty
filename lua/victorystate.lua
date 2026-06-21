ea41503ee5a354a0ba4aa5142cc329faa65401f1
Hater-mater <Hater-mater@users.noreply.github.com>
2026-06-21 15:53:48 +0000

    Code formatting

--- lua/victorystate.lua
+++ lua/victorystate.lua
@@ -1,4 +1,4 @@
 -- Nuke CC reward for Safehouse Raid completion
-Hooks:PostHook(VictoryState , "init", "safehouse_init", function(self, game_state_machine, setup)
+Hooks:PostHook(VictoryState, "init", "safehouse_init", function(self, game_state_machine, setup)
 	self._safehouse_raid_rewarded = true
 end)
