b867f67dfbd17d1fb3864a7e5517e3480d8f3baa
Sokolov49 <81686591+Sokolov49@users.noreply.github.com>
2026-06-21 17:50:15 +0200

    disable securing bags in safehouse raid

--- lua/moneytweakdata.lua
+++ lua/moneytweakdata.lua
@@ -764,6 +764,9 @@ function MoneyTweakData:init(tweak_data)
 	if level_id == "safehouse" then
 		self.small_loot.money_bundle = 50000
 	end
+	if level_id == "chill_combat" or level_id == "chill" then
+		self.bag_values.money = 0
+	end
 
 	self.max_small_loot_value = 20000000
 	self.skilltree = {
