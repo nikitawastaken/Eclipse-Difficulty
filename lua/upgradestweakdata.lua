39acafaf6fac2047e96f2d71c910072f5190a205
nikitawastaken <nikitawastaken@users.noreply.github.com>
2026-06-24 19:36:18 +0000

    Code formatting

--- lua/upgradestweakdata.lua
+++ lua/upgradestweakdata.lua
@@ -1082,7 +1082,7 @@ function UpgradesTweakData:init(tweak_data)
 		{
 			damage_mul_addend = 0.2,
 			max_headshots = 5,
-			lose_on_miss = 2
+			lose_on_miss = 2,
 		},
 	}
 	self.definitions.snp_consecutive_headshots = {
