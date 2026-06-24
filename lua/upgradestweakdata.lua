f19636227ab1f901bc359707239d4cd94076d442
nikitawastaken <59087849+nikitawastaken@users.noreply.github.com>
2026-06-24 22:37:01 +0300

    oopsie forgot a desc value

--- lua/upgradestweakdata.lua
+++ lua/upgradestweakdata.lua
@@ -1116,6 +1116,7 @@ function UpgradesTweakData:init(tweak_data)
 
 	self.skill_descs.single_shot_ammo_return.multibasic = "20%"
 	self.skill_descs.single_shot_ammo_return.multibasic2 = "5"
+	self.skill_descs.single_shot_ammo_return.multibasic3 = "2"
 	self.skill_descs.single_shot_ammo_return.multipro = "2"
 	self.skill_descs.single_shot_ammo_return.multipro2 = "100%"
 	self.skill_descs.single_shot_ammo_return.multipro3 = "4m"
