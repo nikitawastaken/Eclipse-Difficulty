4b0983f5e64df702e3a0d847b0d4a301838442e8
rockymoto517 <rockymoto517@gmail.com>
2026-07-29 17:00:52 -0500

    man

--- lua/clientnetworksession.lua
+++ lua/clientnetworksession.lua
@@ -1,7 +1,7 @@
 --! Potential fix to this being called multiple times
 local env, grading
 Hooks:PostHook(ClientNetworkSession, "notify_host_when_outfits_loaded", "eclipse_post_client_load", function()
-	if Eclipse.current_environment and env ~= Eclipse.current_environment and grading ~= Eclipse.color_grading then
+	if Eclipse.current_environment and (env ~= Eclipse.current_environment or grading ~= Eclipse.color_grading) then
 		Eclipse.utils.client_load_environment(tweak_data.levels[Eclipse.utils.level_id()], Eclipse.current_environment, Eclipse.color_grading)
 		env = Eclipse.current_environment
 		grading = Eclipse.color_grading
