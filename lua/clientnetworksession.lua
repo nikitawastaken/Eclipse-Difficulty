92ba414944f9714d72115ea7693c17747e71ff90
rockymoto517 <rockymoto517@gmail.com>
2026-07-29 16:57:25 -0500

    git commit -am 'commit name'

--- lua/clientnetworksession.lua
+++ lua/clientnetworksession.lua
@@ -1,7 +1,7 @@
----Notably this will run client_load_environment multiple times!
----I have yet to find a way around this, but it's low impact
+--! Potential fix to this being called multiple times
+local env, grading
 Hooks:PostHook(ClientNetworkSession, "notify_host_when_outfits_loaded", "eclipse_post_client_load", function()
-	if Eclipse.current_environment then
+	if Eclipse.current_environment and env ~= Eclipse.current_environment and grading ~= Eclipse.color_grading then
 		Eclipse.utils.client_load_environment(tweak_data.levels[Eclipse.utils.level_id()], Eclipse.current_environment, Eclipse.color_grading)
 	end
 end)
