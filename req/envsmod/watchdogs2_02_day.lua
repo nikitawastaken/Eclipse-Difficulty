39c65c91e2556e07bb7791a60b8fa5316b36c944
Sokolov49 <Sokolov49@users.noreply.github.com>
2026-07-29 22:19:01 +0000

    Code formatting

--- req/envsmod/watchdogs2_02_day.lua
+++ req/envsmod/watchdogs2_02_day.lua
@@ -9,6 +9,5 @@ return {
 	},
 	environment_override = { -- File override
 		["environments/pd2_env_wd2_evening/pd2_env_wd2_evening"] = tostring(Eclipse.mod_path) .. "assets/environments/custom/watchdogs_2_2_day.custom_xml",
-
 	},
 }
