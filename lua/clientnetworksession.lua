a257c14359b3c13ad14015be368bbc2b203ac23d
rockymoto517 <rockymoto517@gmail.com>
2026-07-29 16:58:22 -0500

    I am mentally challenged sometimes often

--- lua/clientnetworksession.lua
+++ lua/clientnetworksession.lua
@@ -3,5 +3,7 @@ local env, grading
 Hooks:PostHook(ClientNetworkSession, "notify_host_when_outfits_loaded", "eclipse_post_client_load", function()
 	if Eclipse.current_environment and env ~= Eclipse.current_environment and grading ~= Eclipse.color_grading then
 		Eclipse.utils.client_load_environment(tweak_data.levels[Eclipse.utils.level_id()], Eclipse.current_environment, Eclipse.color_grading)
+		env = Eclipse.current_environment
+		grading = Eclipse.color_grading
 	end
 end)
