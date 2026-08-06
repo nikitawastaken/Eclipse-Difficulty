c5ab5bf2a2c8a25fe5d893bfbc0641d2af20d3b2
moon <108492539+erushinj@users.noreply.github.com>
2026-08-05 21:04:06 -0600

    Add `CoreWorldInstanceManager:convert_id`

--- lua/coreworldinstancemanager.lua
+++ lua/coreworldinstancemanager.lua
@@ -1,3 +1,19 @@
+-- Since vanilla doesn't keep track of the original IDs...
+function CoreWorldInstanceManager:convert_id(instance_name, id)
+	local instance_data = self:get_instance_data_by_name(instance_name)
+	if not instance_data or not instance_data.start_index then
+		return
+	end
+
+	local continent_data = managers.worlddefinition._continents[instance_data.continent]
+	if not continent_data or not continent_data.base_id then
+		return
+	end
+
+	local converted = continent_data.base_id + self:_get_mod_id(id) + self:start_offset_index() + instance_data.start_index
+	return converted
+end
+
 if Network:is_client() or Global.editor_mode then
 	return
 end
