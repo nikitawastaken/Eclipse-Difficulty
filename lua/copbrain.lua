f5d9ff4096cac7a06a1452db7452f855cb6e7321
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2026-09-15 21:11:34 +0200

    why he's using security logic??????

--- lua/copbrain.lua
+++ lua/copbrain.lua
@@ -4,6 +4,7 @@ CopBrain._logic_variants.chavez_boss = CopBrain._logic_variants.triad_boss
 CopBrain._logic_variants.hector_boss = CopBrain._logic_variants.triad_boss
 CopBrain._logic_variants.drug_lord_boss = CopBrain._logic_variants.triad_boss
 CopBrain._logic_variants.biker_boss = CopBrain._logic_variants.triad_boss
+CopBrain._logic_variants.auctioneer_boss = CopBrain._logic_variants.triad_boss
 CopBrain._logic_variants.fbi_boss = CopBrain._logic_variants.triad_boss
 CopBrain._logic_variants.fbi_female_boss = CopBrain._logic_variants.triad_boss
 CopBrain._logic_variants.city_tank = CopBrain._logic_variants.tank
