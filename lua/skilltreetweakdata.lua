fe4597ed6f3a3d02eb5bdcb81967fc840d44f24d
Hater-mater <swetlaniki@gmail.com>
2026-09-18 13:21:22 +0300

    Human Shield and Controller Freak icons

--- lua/skilltreetweakdata.lua
+++ lua/skilltreetweakdata.lua
@@ -57,7 +57,7 @@ Hooks:PostHook(SkillTreeTweakData, "init", "eclipse_init", function(self, tweak_
 	-- Control Freak
 	self.skills.cable_guy[1].upgrades = { "player_civ_intimidation_mul", "player_civ_calming_alerts" }
 	self.skills.cable_guy[2].upgrades = { "player_intimidate_range_mul", "player_intimidate_aura", "player_intimidation_multiplier" }
-	self.skills.cable_guy.icon_xy = { 0, 0 }
+	self.skills.cable_guy.icon_xy = { 2, 3 }
 
 	-- Stockholm Syndrome
 	self.skills.joker[1].upgrades = { "player_civilian_subdued_indicator" }
@@ -71,7 +71,7 @@ Hooks:PostHook(SkillTreeTweakData, "init", "eclipse_init", function(self, tweak_
 	self.skills.stockholm_syndrome[2].upgrades = { "player_hostage_damage_reduction_addend" }
 	self.skills.stockholm_syndrome.name_id = "menu_human_shield"
 	self.skills.stockholm_syndrome.desc_id = "menu_human_shield_desc"
-	self.skills.stockholm_syndrome.icon_xy = { 0, 0 }
+	self.skills.stockholm_syndrome.icon_xy = { 1, 3 }
 
 	-- Partners in Crime
 	self.skills.control_freak[1].upgrades = { "player_convert_enemies", "player_convert_enemies_max_minions_1", "player_passive_convert_enemies_health_multiplier_1" }
