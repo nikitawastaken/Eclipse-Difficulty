c68b1ea79985d4a5925ddc13742c5ed5c4ff9be2
Sokolov49 <81686591+Sokolov49@users.noreply.github.com>
2026-09-22 14:23:48 +0200

    bhdscanner

--- lua/levelstweakdata.lua
+++ lua/levelstweakdata.lua
@@ -834,20 +834,18 @@ Hooks:PostHook(LevelsTweakData, "init", "eclipse_init", function(self)
 		["watchdogs1_03_day"] = 3,
 		["watchdogs1_04_evening"] = 2,
 		["watchdogs1_05_evening"] = 2,
-		["default"] = 1,
 	}
 	self.watchdogs_1_night.env_params.environment = nil
 	self.watchdogs_1_night.random_environments = {
 		["watchdogs1_01_night"] = 2,
 		["watchdogs1_02_night"] = 2,
 		["watchdogs1_03_night"] = 2,
-		["default"] = 1,
 	}
 	self.watchdogs_2.random_environments = {
-		["watchdogs2_02_night"] = 2,
+		["watchdogs2_02_night_prebeta"] = 4,
 		["watchdogs2_02_night_bhdscanner"] = 2,
 		["watchdogs2_02_night_matrix"] = 2,
-		["watchdogs2_02_night_sunset"] = 2,
+		["watchdogs2_02_night_sunset"] = 1,
 	}
 	-- self.watchdogs_2_day.env_params.environment = nil
 	self.watchdogs_2_day.random_environments = {
---
+++ req/envsmod/watchdogs2_02_night_prebeta.lua
@@ -0,0 +1,62 @@
+return {
+	flashlights_on = true, -- Flashlights
+	saturation_value = -0.2,
+	color_grading = { -- Randomized color gradings
+		"color_bhd_classic",
+		"color_payday_classic",
+		"color_heat_classic",
+		"color_xxxgen",
+		"color_xgen",
+	},
+	environment_override = { -- File override
+		["environments/pd2_env_night/pd2_env_night"] = tostring(Eclipse.mod_path) .. "assets/environments/custom/watchdogs_2_night_darkngriddy.custom_xml",
+	},
+	effect_spawner = { -- Fog effects and such
+		["effects/envsmod/night_time/fog_very_faint_bright_white"] = {
+			{
+				position = Vector3(-1993, 109, 36),
+				rotation = Rotation(0, 0, -0)
+			},
+			{
+				position = Vector3(-1993, 3972, 36),
+				rotation = Rotation(0, 0, -0)
+			},
+			{
+				position = Vector3(-1993, 6040, 36),
+				rotation = Rotation(0, 0, -0)
+			},
+			{
+				position = Vector3(-1993, 7748, 36),
+				rotation = Rotation(0, 0, -0)
+			},
+			{
+				position = Vector3(-4174, -14, 36),
+				rotation = Rotation(0, 0, -0)
+			},
+			{
+				position = Vector3(-6599, -14, 36),
+				rotation = Rotation(0, 0, -0)
+			},
+			{
+				position = Vector3(-2073, -2357, 36),
+				rotation = Rotation(0, 0, -0)
+			},
+			{
+				position = Vector3(-2073, -4638, 36),
+				rotation = Rotation(0, 0, -0)
+			},
+			{
+				position = Vector3(-2073, -6127, 36),
+				rotation = Rotation(0, 0, -0)
+			},
+			{
+				position = Vector3(644, -956, 82),
+				rotation = Rotation(0, 0, -0)
+			},
+			{
+				position = Vector3(364, 706, 82),
+				rotation = Rotation(0, 0, -0)
+			}
+		}
+	},
+}
