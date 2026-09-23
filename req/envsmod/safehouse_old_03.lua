4b39e255d8809e63f71d5499716df571617ef434
Sokolov49 <81686591+Sokolov49@users.noreply.github.com>
2026-09-23 12:27:42 +0200

    add birds to safehouse and reduce bloom

--- req/envsmod/safehouse_old_03.lua
+++ req/envsmod/safehouse_old_03.lua
@@ -6,4 +6,12 @@ return {
 	environment_override = { -- File override
 		["environments/pd2_env_mid_day/pd2_env_mid_day"] = tostring(Eclipse.mod_path) .. "assets/environments/custom/safehouse_old_3.custom_xml",
 	},
+	effect_spawner = { -- Fog effects and such
+		["effects/envsmod/birds"] = {
+			{
+				position = Vector3(-5518, -525, 1694),
+				rotation = Rotation(50, 0, 0),
+			},
+		},
+	},
 }
